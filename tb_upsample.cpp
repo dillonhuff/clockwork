a6f92ec2a2e24a77566b137719158ca1e8be2dcb
Dillon Huff <dillonhuff@gmail.com>
2020-01-20 20:55:15 -0800

    Created upsample test

--- tb_upsample.cpp
+++ tb_upsample.cpp
@@ -1,3 +1,22 @@
+#include "upsample.h"
+
+#include <iostream>
+
+using namespace std;
+
 int main() {
-  return 1;
+
+  InputStream write0;
+  OutputStream read0;
+
+  for (int i = 0; i < 10; i++) {
+    write0.write(i);
+  }
+  upsample(read0, write0);
+
+  for (int i = 0; i < 10; i++) {
+    assert(read0.read() == i);
+    assert(read0.read() == i);
+  }
+  return 0;
 }
