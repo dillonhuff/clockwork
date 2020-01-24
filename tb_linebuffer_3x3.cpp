c7a416e195d7492862b0426e2d0ed4325d06c303
Dillon Bailey Huff <dhuff@kiwi.stanford.edu>
2020-01-23 18:03:54 -0800

    Tests pass with II = 1

--- tb_linebuffer_3x3.cpp
+++ tb_linebuffer_3x3.cpp
@@ -6,9 +6,25 @@ using namespace std;
 
 int main() {
   InputStream write0;
+  vector<int> img;
   for (int r = 0; r < 64; r++) {
     for (int c = 0; c < 64; c++) {
       write0.write(r*64 + c);
+      img.push_back(r*64 + c);
+    }
+  }
+
+  vector<int> expected;
+  for (int r = 0; r < 62; r++) {
+    for (int c = 0; c < 62; c++) {
+
+      int res = 0;
+      for (int ri = 0; ri < 3; ri++) {
+        for (int ci = 0; ci < 3; ci++) {
+          res += img.at((r + ri)* 64 + c + ci);
+        }
+      }
+      expected.push_back(res);
     }
   }
 
@@ -34,8 +50,11 @@ int main() {
       read8,
       write0);
 
+
   for (int v = 0; v < 62*62; v++) {
-    read0.read();
+    int rv = read0.read();
+    assert(rv == expected.at(v));
+    cout << "rv[" << v << "] = " << rv << endl;
     //read1.read();
     //read2.read();
     //read3.read();
