4cde8b78e3d15899f5d4b661c0f4884664289028
Dillon Huff <dillonhuff@gmail.com>
2020-01-25 20:30:34 -0800

    Wrapping up stream creation

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -61,6 +61,11 @@ class UBuffer {
     
     isl_map* physical_address_mapping;
 
+    std::string bundle_stream(const std::string& bundle_name) {
+      bool input_bundle = isIn.at(pick(port_bundles.at(bundle_name)));
+      return string(input_bundle ? "Input" : "Output") + "Stream<int>& " + bundle_name;
+    }
+
     isl_union_map* global_schedule() {
       umap* s = isl_union_map_read_from_str(ctx, "{ }");
       for (auto other : schedule) {
@@ -147,46 +152,6 @@ class UBuffer {
         isl_union_map_read_from_str(ctx, sched.c_str());
     }
 
-    isl_basic_set* build_domain(const std::vector<int>& ranges) {
-      assert(ranges.size() % 2 == 0);
-
-      isl_basic_set* bset = isl_basic_set_universe(cpy(space));
-      isl_constraint* c;
-      isl_local_space* ls = isl_local_space_from_space(space);
-
-      for (size_t i = 0; i < ranges.size(); i += 2) {
-        int start = ranges.at(i);
-        int end = ranges.at(i + 1);
-        int varNum = i / 2;
-
-        c = isl_constraint_alloc_inequality(cpy(ls));
-        c = isl_constraint_set_constant_si(c, -start);
-        c = isl_constraint_set_coefficient_si(c, isl_dim_set, varNum, 1);
-        bset = isl_basic_set_add_constraint(bset, c);
-
-        c = isl_constraint_alloc_inequality(cpy(ls));
-        c = isl_constraint_set_constant_si(c, end);
-        c = isl_constraint_set_coefficient_si(c, isl_dim_set, varNum, -1);
-        bset = isl_basic_set_add_constraint(bset, c);
-      }
-
-      return bset;
-    }
-
-    isl_map* build_map(const int offset) {
-      isl_basic_map* m = isl_basic_map_universe(cpy(map_space));
-      isl_local_space* ls = isl_local_space_from_space(map_space);
-
-      int varNum = 0;
-      isl_constraint* c = isl_constraint_alloc_equality(cpy(ls));
-      c = isl_constraint_set_constant_si(c, -offset);
-      c = isl_constraint_set_coefficient_si(c, isl_dim_out, varNum, 1);
-      c = isl_constraint_set_coefficient_si(c, isl_dim_in, varNum, -1);
-      m = isl_basic_map_add_constraint(m, c);
-
-      return isl_map_from_basic_map(m);
-    }
-
     vector<string> get_in_bundles() const {
       vector<string> outpts;
       for (auto m : port_bundles) {
@@ -226,285 +191,7 @@ class UBuffer {
       assert(false);
     }
 
-    void add_out_port(const std::string& name, const std::vector<int>& ranges) {
-      isIn[name] = false;
-      domain[name] = isl_set_from_basic_set(build_domain(ranges));
-    }
-
 };
-
-isl_constraint* eq(isl_space* const space, const int value, const std::vector<int>& in_coeffs, const std::vector<int>& out_coeffs) {
-  isl_local_space* ls = isl_local_space_from_space(space);
-  isl_constraint* c = isl_constraint_alloc_equality(cpy(ls));
-  c = isl_constraint_set_constant_si(c, -value);
-  for (size_t i = 0; i < in_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_in, i, -in_coeffs[i]);
-  }
-  for (size_t i = 0; i < out_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_out, i, out_coeffs[i]);
-  }
-
-  return c;
-}
-
-isl_constraint* le(isl_space* const space, const int value, const std::vector<int>& in_coeffs, const std::vector<int>& out_coeffs) {
-  isl_local_space* ls = isl_local_space_from_space(space);
-  isl_constraint* c = isl_constraint_alloc_inequality(cpy(ls));
-  c = isl_constraint_set_constant_si(c, value);
-  for (size_t i = 0; i < in_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_in, i, -in_coeffs[i]);
-  }
-  for (size_t i = 0; i < out_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_out, i, out_coeffs[i]);
-  }
-
-  return c;
-}
-
-isl_constraint* ge(isl_space* const space, const int value, const std::vector<int>& in_coeffs, const std::vector<int>& out_coeffs) {
-  isl_local_space* ls = isl_local_space_from_space(space);
-  isl_constraint* c = isl_constraint_alloc_inequality(cpy(ls));
-  c = isl_constraint_set_constant_si(c, value);
-  for (size_t i = 0; i < in_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_in, i, in_coeffs[i]);
-  }
-  for (size_t i = 0; i < out_coeffs.size(); i++) {
-    c = isl_constraint_set_coefficient_si(c, isl_dim_out, i, -out_coeffs[i]);
-  }
-
-  return c;
-}
-
-// What questions do I have about spaces / variable names by index?
-//  1. is a copy of a space equal to the original space?
-//  2. What are these tuple things that keep getting mentioned?
-//  3. Can you intersect sets from different spaces?
-//  4. When sets from different spaces are operated on w/named variables
-//     are constraints merged by variable names or indexes?
-//  5. What is the space of the inverse of a map? Does it make a new space
-//     with in and out variables reversed?
-void test_swizzle_buffer() {
-
-  struct isl_ctx *ctx;
-
-  ctx = isl_ctx_alloc();
-
-  // 2 image variables
-  // time
-  // physical position variable
-  UBuffer buf;
-
-  // Logical addresses
-
-  // Add ports, say two in, three out
-  // Want to add read / write schedules, and domains
-  // Should the domain describe all logical addresses
-  // as a restriction of r / c
-  // Or should it describe all logical addresses as
-  // the union of the ranges of all address write functions
-  buf.ctx = ctx;
-  buf.space = isl_space_set_alloc(ctx, 0, 2);
-  buf.map_space = isl_space_alloc(ctx, 0, 2, 3);
-
-
-  buf.physical_address_mapping =
-    isl_map_from_basic_map(isl_basic_map_universe(cpy(buf.map_space)));
-
-  // Mapping: (x, y) -> (x, y % 3)
-  // How to represent modulus? y % 3 === k, exists q. 3*q + k = y
-  //int x = 0;
-  //int y = 1;
-
-  //int k = 0;
-  //int k = 1;
-  //int q = 2;
-
-  // How do you create a space if you dont know how many
-  // variables you will need?
-  auto c = eq(buf.map_space, 0, {1}, {1});
-  buf.physical_address_mapping = isl_map_add_constraint(buf.physical_address_mapping, c);
-
-  c = eq(buf.map_space, 0, {0, 1}, {0, 1, 3});
-  buf.physical_address_mapping = isl_map_add_constraint(buf.physical_address_mapping, c);
-  
-  c = ge(buf.map_space, 2, {0, 0}, {0, 1, 0});
-  buf.physical_address_mapping = isl_map_add_constraint(buf.physical_address_mapping, c);
-  
-  cout << "Physical address mapping" << endl;
-  print(ctx, buf.physical_address_mapping);
-  cout << endl;
-  
-  c = le(buf.map_space, 0, {0, 0}, {0, 1, 0});
-  buf.physical_address_mapping = isl_map_add_constraint(buf.physical_address_mapping, c);
-  
-  cout << "Physical address mapping" << endl;
-  print(ctx, buf.physical_address_mapping);
-  cout << endl;
-
-  buf.physical_address_mapping = isl_map_project_out(buf.physical_address_mapping, isl_dim_out, 2, 1);
-
-  cout << "Physical address mapping" << endl;
-  print(ctx, buf.physical_address_mapping);
-  cout << endl;
-
-  // Now: Create a domain for input / output ports
-
-  isl_ctx_free(ctx);
-}
-
-void basic_space_tests() {
-
-  struct isl_ctx *ctx;
-  ctx = isl_ctx_alloc();
-
-
-
-  auto s0 = isl_space_set_alloc(ctx, 0, 2);
-  auto s1 = isl_space_set_alloc(ctx, 0, 2);
-  auto s2 = isl_space_set_alloc(ctx, 0, 3);
-
-  // A space is equal to itself
-  assert(isl_space_is_equal(s0, s0));
-
-  auto copyS0 = cpy(s0);
-
-  // A space is equal to a copy of itself
-  assert(isl_space_is_equal(s0, copyS0));
-
-  // A space is equal to another space with the same construction params
-  assert(isl_space_is_equal(s0, s1));
-
-  // A space is not equal to another space with different numbers of
-  // variables
-  assert(!isl_space_is_equal(s0, s2));
-
-  auto set0 = isl_basic_set_universe(s0);
-  auto set2 = isl_basic_set_universe(s2);
-
-  cout << "s0 before naming..." << endl;
-  print(ctx, s0);
-
-  s0 = isl_space_set_dim_name(s0, isl_dim_set, 0, "x");
-  s0 = isl_space_set_dim_name(s0, isl_dim_set, 1, "y");
-  s0 = isl_space_set_tuple_name(s0, isl_dim_set, "Q");
-
-  cout << "s0 after naming..." << endl;
-  print(ctx, s0);
-
-  // This fails because set0 is not equal to set2
-  //auto res = isl_basic_set_intersect(set0, set2);
-  //assert(false);
-  //
-  // So maybe what we need is several different spaces
-  //  - Logical address space (set space)
-  //  - Physical address space (set space)
-  //  - Schedule space (la -> t)
-  //  - PMap space (la -> pa) (what about extra variables?)
-  //  To add extra variables to remove via projection we need
-  //   to insert variables in the local space
-
-  isl_ctx_free(ctx);
-}
-
-//void ubuffer_test() {
-  //struct isl_ctx *ctx;
-
-  //ctx = isl_ctx_alloc();
-
-  //UBuffer buf;
-  //buf.ctx = ctx;
-  //buf.space = isl_space_set_alloc(ctx, 0, 1);
-  //buf.map_space = isl_space_alloc(ctx, 0, 1, 1);
-
-  //buf.isIn["in"] = true;
-  //buf.domain["in"] = isl_set_from_basic_set(buf.build_domain({0, 7}));
-  //buf.schedule["in"] = buf.build_map(0);
-  
-  //buf.add_out_port("out0", {0, 5});
-  //buf.schedule["out0"] = buf.build_map(2);
-  //buf.add_out_port("out1", {1, 6});
-  //buf.schedule["out1"] = buf.build_map(1);
-  //buf.add_out_port("out2", {2, 7});
-  //buf.schedule["out2"] = buf.build_map(0);
-
-  //cout << "--- Domains" << endl;
-  //for (auto d : buf.domain) {
-    //cout << "Set for: " << d.first << endl;
-    //print(buf.ctx, d.second);
-  //}
-
-  //cout << "--- Schedules" << endl;
-  //for (auto d : buf.schedule) {
-    //cout << "Schedule for: " << d.first << endl;
-    //print(buf.ctx, d.second);
-  //}
-
-  //cout << "Adding domains to schedules..." << endl;
-  //for (auto d : buf.schedule) {
-    //isl_set* s = buf.domain.at(d.first);
-    //isl_map* m = isl_map_intersect_domain(cpy(d.second), cpy(s));
-    //cout << "Schedule with domain for " << d.first << endl;
-    //print(buf.ctx, m);
-    //cout << "Cardinality of domain..." << endl;
-    //print(buf.ctx, isl_set_card(cpy(s)));
-  //}
-
-  //string in_name = buf.get_in_port();
-  //isl_map* in_sched = buf.schedule.at(in_name);
-  //isl_set* in_domain = buf.domain.at(in_name);
-  //isl_pw_multi_aff* write_time =
-    //isl_map_lexmax_pw_multi_aff(in_sched);
-  //cout << "Write times..." << endl;
-  //print(ctx, write_time);
-  //for (auto d : buf.isIn) {
-    //if (!d.second) {
-      //string out_name = d.first;
-      //cout << "Output port: " << d.first << endl;
-      //isl_map* out_sched = buf.schedule.at(out_name);
-      //isl_pw_multi_aff* first_read_time =
-        //isl_map_lexmin_pw_multi_aff(out_sched);
-
-      //auto diff =
-        //isl_pw_multi_aff_sub(cpy(first_read_time), cpy(write_time));
-
-      //cout << "Write -> read Difference..." << endl;
-      //print(ctx, diff);
-
-      //auto diff_map =
-        //isl_map_from_pw_multi_aff(diff);
-      
-      //isl_set* out_domain = buf.domain.at(out_name);
-      //diff_map = isl_map_intersect_domain(diff_map, cpy(out_domain));
-      //auto range = isl_map_range(diff_map);
-
-      //// Intersect with range?
-      //isl_basic_set* less_value = isl_basic_set_universe(cpy(buf.space));
-      //isl_local_space* ls = isl_local_space_from_space(buf.space);
-      //isl_constraint* c = isl_constraint_alloc_inequality(cpy(ls));
-      //c = isl_constraint_set_constant_si(c, -1);
-      //c = isl_constraint_set_coefficient_si(c, isl_dim_set, 0, -1);
-      //less_value = isl_basic_set_add_constraint(less_value, c);
-
-      //cout << "LTZ..." << endl;
-      //print(buf.ctx, less_value);
-
-      //range = isl_set_intersect(isl_set_from_basic_set(less_value), range);
-
-      //cout << "Range of output..." << endl;
-      //print(ctx, range);
-
-      //if (isl_set_is_empty(range)) {
-        //cout << "\tPassed, no violated deps" << endl;
-      //} else {
-        //cout << "\tERROR: Some values read before being written" << endl;
-        //assert(false);
-      //}
-    //}
-  //}
-
-  //isl_ctx_free(buf.ctx);
-
-//}
 
 isl_stat get_const(isl_set* s, isl_qpolynomial* qp, void* user) {
   vector<int>* vals = (vector<int>*) user;
@@ -1122,8 +809,6 @@ void generate_hls_code(UBuffer& buf) {
       regex re(b.first + "(.*);");
       string inpt = pick(b.second);
       code_string = regex_replace(code_string, re, inpt + "_write(" + b.first + ", " + inpt + "_delay);");
-      //code_string = regex_replace(code_string, re, b.first + "_bundle_action(" + b.first + ", " + delay_list + ");");
-      //inpt + "_delay);");
     }
   }
   for (auto b : buf.port_bundles) {
@@ -1145,7 +830,6 @@ void generate_hls_code(UBuffer& buf) {
 
   out << endl << endl;
   for (auto inpt : buf.get_in_ports()) {
-  //for (auto inpt : buf.get_in_bundles()) {
     out << "inline void " << inpt << "_write(" << "InputStream<int>& " << inpt << ", " << inpt + "_cache& " << inpt << "_delay) {" << endl;
     out << "\tint " + inpt + "_value = " + inpt + ".read(); " + inpt + "_delay.push(" + inpt + "_value);" << endl;
     out << "}" << endl << endl;
@@ -1300,8 +984,9 @@ void generate_hls_code(UBuffer& buf) {
   out << "void " << buf.name << "(";
   size_t nargs = 0;
   for (auto pt : buf.port_bundles) {
-    bool input_bundle = buf.isIn.at(*begin(pt.second));
-    out << (input_bundle ? "Input" : "Output") << "Stream<int>& " << pt.first << endl;
+    //bool input_bundle = buf.isIn.at(*begin(pt.second));
+    //out << (input_bundle ? "Input" : "Output") << "Stream<int>& " << pt.first << endl;
+    out << buf.bundle_stream(pt.first);
     if (nargs < buf.port_bundles.size() - 1) {
       out << ", ";
     }
@@ -1321,8 +1006,9 @@ void generate_hls_code(UBuffer& buf) {
   of << "void " << buf.name << "(";
   nargs = 0;
   for (auto pt : buf.port_bundles) {
-    bool input_bundle = buf.isIn.at(*begin(pt.second));
-    of << (input_bundle ? "Input" : "Output") << "Stream<int>& " << pt.first << endl;
+    //bool input_bundle = buf.isIn.at(*begin(pt.second));
+    //of << (input_bundle ? "Input" : "Output") << "Stream<int>& " << pt.first << endl;
+    of << buf.bundle_stream(pt.first);
     if (nargs < buf.port_bundles.size() - 1) {
       of << ", ";
     }
@@ -1703,7 +1389,6 @@ struct op {
       auto nds = active_vecs;
       cout << "At loop: " << this->name << endl;
       assert(nds.size() > 0);
-      //nds.at(nds.size() - 1) = to_string(stoi(nds.at(nds.size() - 1)) + 1);
 
       nds.push_back(name);
       nds.push_back("0");
