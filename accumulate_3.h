862ffcf7f2387fff4330a6d80bd4d025ee0c2bd8
Dillon Huff <dillonhuff@gmail.com>
2020-01-26 19:10:30 -0800

    Deleted some code

--- accumulate_3.h
+++ accumulate_3.h
@@ -14,18 +14,4 @@ int accumulate_3(hw_uint<96>& in) {
   int res = (a + b + c).to_int();
   cout << "res = " << res << endl;
   return res;
-  //cout << "Value = " << in.val << endl;
-  //int vals[3];
-  //for (int i = 0; i < 3; i++) {
-    //int start = i*32;
-    //bsim::static_quad_value_bit_vector<32> bv;
-    //for (int p = 0; p < 32; p++) {
-      //bv.set(p, in.val.get(start + p));
-    //}
-
-    //cout << "\t" << bv.to_type<int>() << endl;
-    //vals[i] = bv.to_type<int>();
-
-  //}
-  //return vals[0] + vals[1] + vals[2];
 }
