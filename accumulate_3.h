8fb89c18476e833ce498d366730a6fc6a17cbe5e
Dillon Huff <dillonhuff@gmail.com>
2020-01-26 18:28:10 -0800

    tiny example of conv running

--- accumulate_3.h
+++ accumulate_3.h
@@ -5,5 +5,17 @@
 static inline
 int accumulate_3(hw_uint<96>& in) {
   cout << "Value = " << in.val << endl;
-  return 123;
+  int vals[3];
+  for (int i = 0; i < 3; i++) {
+    int start = i*32;
+    bsim::static_quad_value_bit_vector<32> bv;
+    for (int p = 0; p < 32; p++) {
+      bv.set(p, in.val.get(start + p));
+    }
+
+    cout << "\t" << bv.to_type<int>() << endl;
+    vals[i] = bv.to_type<int>();
+
+  }
+  return vals[0] + vals[1] + vals[2];
 }
---
+++ tb_conv_1d.cpp
@@ -0,0 +1,18 @@
+#include "conv_1d.h"
+
+int main() {
+  HWStream<int> in;
+  HWStream<int> out;
+
+  for (int i = 0; i < 10; i++) {
+    in.write(i);
+  }
+
+  conv_1d(in, out);
+
+  for (int i = 0; i < 10 - 2; i++) {
+    int v = out.read();
+    cout << "v[" << i << "] = " << v << endl;
+    assert(v == i + i + 1 + i + 2);
+  }
+}
