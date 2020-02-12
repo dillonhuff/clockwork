f5aa96720f23e1e0f5b282db21470ec643072b45
Dillon Huff <dillonhuff@gmail.com>
2020-02-12 12:56:50 -0800

    Added tcl script with arguments

--- soda_codes/common/build_soda.sh
+++ soda_codes/common/build_soda.sh
@@ -2,5 +2,6 @@ app=$1
 ~/soda-compiler/src/sodac $app.soda --xocl-kernel soda_$app.cpp
 ~/soda-compiler/src/sodac $app.soda --xocl-header soda_$app.h
 ~/soda-compiler/src/sodac $app.soda --xocl-host soda_host.cpp
-vivado_hls -f soda_${app}_hls.tcl
+vivado_hls -f ../common/soda_hls.tcl -tclargs $1
+
 
