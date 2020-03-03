083dbef0bf5212dbe70dc5dab70b577aab4f642b
Dillon Huff <dillonhuff@gmail.com>
2020-03-02 18:33:14 -0800

    Added stream test files

--- make_code.sh
+++ make_code.sh
@@ -1,4 +1,4 @@
-BARVINOK_PATH=/Users/dillon/Downloads/barvinok-0.41/isl/
+BARVINOK_PATH=/Users/dillon/Downloads/barvinok-0.41/isl
 ISL_PATH=/Users/dillon/Downloads/barvinok-0.41/isl/
 OPT_PATH=/opt/include
 OPT_LIB_PATH=/opt/lib
