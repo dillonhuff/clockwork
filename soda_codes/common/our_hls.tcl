e96939c9aab75237e0240b35b8675667b4a3d1e6
Dillon Huff <dillonhuff@gmail.com>
2020-03-14 14:29:26 -0700

    Can now run a simple benchmark

--- soda_codes/common/our_hls.tcl
+++ soda_codes/common/our_hls.tcl
@@ -6,9 +6,9 @@ set ClockPeriod 5.0
 open_project -reset ${KernelName}_proj
 set_top ${KernelName}
 add_files -cflags "-std=c++11" ${KernelName}.cpp
-add_files -cflags "-I ../../ -std=c++11 -D__VIVADO_SYNTH__" ${KernelName}.cpp
+add_files -cflags "-I ../../../ -std=c++11 -D__VIVADO_SYNTH__" ${KernelName}.cpp
 
-add_files -cflags "-I ../.. -std=c++11 -D__VIVADO_SYNTH__" -tb regression_tb_${KernelName}.cpp
+add_files -cflags "-I ../../.. -std=c++11 -D__VIVADO_SYNTH__" -tb regression_tb_${KernelName}.cpp
 
 open_solution -reset "solution1"
 set_part $Device
