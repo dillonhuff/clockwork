e0fba44ac7cc9d1c603e1b8eca7ff47e9fa86b73
Dillon Huff <dillonhuff@gmail.com>
2020-03-18 01:15:24 -0700

    Experimenting with upsample

---
+++ upsample_stencil_opt.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+#include "hw_classes.h"
+
+void upsample_stencil_opt(HWStream<hw_uint<32> >& /* get_args num ports = 1 */Img_off, HWStream<hw_uint<32> >& /* get_args num ports = 1 */upsample_stencil);
