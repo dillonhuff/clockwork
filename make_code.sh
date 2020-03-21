9214835223f0f78ac452953a3fb7a84372322fe5
Dillon Huff <dillonhuff@gmail.com>
2020-03-21 16:56:20 -0700

    Removed useless files

--- make_code.sh
+++ make_code.sh
@@ -1,4 +1,4 @@
 source ./user_settings/dhuff_settings.sh
-$CXX -std=c++11 build_set_test.cpp app.cpp isl_utils.cpp -I $BARVINOK_PATH -L $ISL_PATH -lbarvinok -lisl -I $OPT_PATH -lbarvinok -lgmp -L $OPT_LIB_PATH -lntl -lpolylibgmp
+$CXX -std=c++11 build_set_test.cpp app.cpp isl_utils.cpp ubuffer.cpp -I $BARVINOK_PATH -L $ISL_PATH -lbarvinok -lisl -I $OPT_PATH -lbarvinok -lgmp -L $OPT_LIB_PATH -lntl -lpolylibgmp
 
 
