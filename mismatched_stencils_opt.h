6bee62b6c41389a957e1a6383b7a2c48735abb0d
Dillon Huff <dillonhuff@gmail.com>
2020-03-21 17:06:33 -0700

    Removed more dead files

--- mismatched_stencils_opt.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "hw_classes.h"
-
-void mismatched_stencils_opt(HWStream<hw_uint<32> >& /* get_args num ports = 1 */off_chip_img0, HWStream<hw_uint<32> >& /* get_args num ports = 1 */off_chip_img1, HWStream<hw_uint<32> >& /* get_args num ports = 1 */mismatched_stencils);
