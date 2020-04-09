af87ada224203d1efea1f761d394702a8d814c0c
joyliu37 <joyliu37@sina.com>
2020-04-09 00:27:45 -0700

    rm file

--- gray_naive.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "hw_classes.h"
-
-void gray_naive(HWStream<hw_uint<32> >& /* get_args num ports = 1 */Img_off, HWStream<hw_uint<32> >& /* get_args num ports = 1 */gray);
--- t0_naive.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "hw_classes.h"
-
-void t0_naive(HWStream<hw_uint<32> >& /* get_args num ports = 1 */t1_arg, HWStream<hw_uint<32> >& /* get_args num ports = 1 */t0);
