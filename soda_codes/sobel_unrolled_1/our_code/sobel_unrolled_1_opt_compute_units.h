2df692294958757fec79b16ebb8f527fa8a124e2
Dillon Huff <dillonhuff@gmail.com>
2020-04-19 11:37:22 -0700

    Actually ran sobel

--- soda_codes/sobel_unrolled_1/our_code/sobel_unrolled_1_opt_compute_units.h
+++ soda_codes/sobel_unrolled_1/our_code/sobel_unrolled_1_opt_compute_units.h
@@ -14,7 +14,7 @@ hw_uint<32>  id_unrolled_1(hw_uint<32>& off_chip_img) {
 }
 
   // mag_y_update_0 unroll factor: 1
-hw_uint<32>  sobel_mx_unrolled_1(hw_uint<192>& img) {
+hw_uint<32>  sobel_my_unrolled_1(hw_uint<192>& img) {
   hw_uint<32> whole_result;
   hw_uint<192> lane_0_img;
   // Need offset: -1, -1
@@ -26,6 +26,27 @@ hw_uint<32>  sobel_mx_unrolled_1(hw_uint<192>& img) {
   // Need offset: 0, 1
   set_at<96, 192>(lane_0_img, img.extract<96, 127>());
   // Need offset: 1, -1
+  set_at<128, 192>(lane_0_img, img.extract<128, 159>());
+  // Need offset: 1, 1
+  set_at<160, 192>(lane_0_img, img.extract<160, 191>());
+  auto result_0 = sobel_my(lane_0_img);
+  set_at<0, 32>(whole_result, result_0);
+  return whole_result;
+}
+
+  // mag_x_update_0 unroll factor: 1
+hw_uint<32>  sobel_mx_unrolled_1(hw_uint<192>& img) {
+  hw_uint<32> whole_result;
+  hw_uint<192> lane_0_img;
+  // Need offset: -1, -1
+  set_at<0, 192>(lane_0_img, img.extract<0, 31>());
+  // Need offset: -1, 0
+  set_at<32, 192>(lane_0_img, img.extract<32, 63>());
+  // Need offset: -1, 1
+  set_at<64, 192>(lane_0_img, img.extract<64, 95>());
+  // Need offset: 1, -1
+  set_at<96, 192>(lane_0_img, img.extract<96, 127>());
+  // Need offset: 1, 0
   set_at<128, 192>(lane_0_img, img.extract<128, 159>());
   // Need offset: 1, 1
   set_at<160, 192>(lane_0_img, img.extract<160, 191>());
