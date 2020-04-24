63fc48dbe958cd7807f54eb0b8e37a55684de65d
Cloud User <centos@ip-172-31-31-20.ec2.internal>
2020-04-24 18:17:09 +0000

    changes

--- soda_codes/blur_xy_16_unrolled_1_opt/our_code/blur_xy_16_unrolled_1_opt.cpp
+++ soda_codes/blur_xy_16_unrolled_1_opt/our_code/blur_xy_16_unrolled_1_opt.cpp
@@ -285,6 +285,20 @@ inline void input_input_update_0_write_bundle_write(hw_uint<16>& input_update_0_
 
 
 // Operation logic
+inline void input_update_0(HWStream<hw_uint<16> >& /* buffer_args num ports = 1 */input_arg, input_cache& input, int d0, int d1) {
+	// Consume: input_arg
+	auto input_arg_0_c__0_value = input_arg.read();
+	auto compute_result = input_generated_compute_unrolled_1(input_arg_0_c__0_value);
+	// Produce: input
+	input_input_update_0_write_bundle_write(compute_result, input, d0, d1);
+#ifndef __VIVADO_SYNTH__
+  hw_uint<16> debug_compute_result(compute_result);
+  hw_uint<16> debug_compute_result_lane_0;
+  set_at<0, 16, 16>(debug_compute_result_lane_0, debug_compute_result.extract<0, 15>());
+  *global_debug_handle << "input_update_0," << (1*d0 + 0) << ", " << d1<< "," <<  debug_compute_result_lane_0 << endl;
+#endif //__VIVADO_SYNTH__
+}
+
 inline void blur_xy_16_unrolled_1_update_0(blurx_cache& blurx, HWStream<hw_uint<16> >& /* buffer_args num ports = 1 */blur_xy_16_unrolled_1, int d0, int d1) {
 	// Consume: blurx
 	auto blurx_0_c__0_value = blurx_blur_xy_16_unrolled_1_update_0_read_bundle_read(blurx/* source_delay */, d0, d1);
@@ -316,20 +330,6 @@ inline void blurx_update_0(input_cache& input, blurx_cache& blurx, int d0, int d
   hw_uint<16> debug_compute_result_lane_0;
   set_at<0, 16, 16>(debug_compute_result_lane_0, debug_compute_result.extract<0, 15>());
   *global_debug_handle << "blurx_update_0," << (1*d0 + 0) << ", " << d1<< "," <<  debug_compute_result_lane_0 << endl;
-#endif //__VIVADO_SYNTH__
-}
-
-inline void input_update_0(HWStream<hw_uint<16> >& /* buffer_args num ports = 1 */input_arg, input_cache& input, int d0, int d1) {
-	// Consume: input_arg
-	auto input_arg_0_c__0_value = input_arg.read();
-	auto compute_result = input_generated_compute_unrolled_1(input_arg_0_c__0_value);
-	// Produce: input
-	input_input_update_0_write_bundle_write(compute_result, input, d0, d1);
-#ifndef __VIVADO_SYNTH__
-  hw_uint<16> debug_compute_result(compute_result);
-  hw_uint<16> debug_compute_result_lane_0;
-  set_at<0, 16, 16>(debug_compute_result_lane_0, debug_compute_result.extract<0, 15>());
-  *global_debug_handle << "input_update_0," << (1*d0 + 0) << ", " << d1<< "," <<  debug_compute_result_lane_0 << endl;
 #endif //__VIVADO_SYNTH__
 }
 
@@ -385,14 +385,14 @@ void blur_xy_16_unrolled_1_opt(HWStream<hw_uint<16> >& /* get_args num ports = 1
 
 extern "C" {
 
-static void read_input(hw_uint<16>* input, hls::stream<hw_uint<16>>& v, const int size) {
+static void read_input(hw_uint<16>* input, hls::stream<hw_uint<16> >& v, const int size) {
   for (int i = 0; i < INPUT_SIZE; i++) {
     #pragma HLS pipeline II=1
     v.write(input[i]);
   }
 }
 
-static void write_output(hw_uint<16>* output, hls::stream<hw_uint<16>>& v, const int size) {
+static void write_output(hw_uint<16>* output, hls::stream<hw_uint<16> >& v, const int size) {
   for (int i = 0; i < OUTPUT_SIZE; i++) {
     #pragma HLS pipeline II=1
     output[i] = v.read();
@@ -409,14 +409,14 @@ void blur_xy_16_unrolled_1_opt_accel(hw_uint<16>* input_update_0_read, hw_uint<1
 #pragma HLS INTERFACE s_axilite port = size bundle = control
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
-  static hls::stream<hw_uint<16>> input_arg_update_0_write_channel;
-  static hls::stream<hw_uint<16>> blur_xy_16_unrolled_1_update_0_write_channel;
+  static hls::stream<hw_uint<16> > input_arg_update_0_write_channel;
+  static hls::stream<hw_uint<16> > blur_xy_16_unrolled_1_update_0_write_channel;
 
   read_input(input_arg_update_0_write, input_arg_update_0_write_channel, size);
 
   blur_xy_16_unrolled_1_opt(input_update_0_read_channel, blur_xy_16_unrolled_1_update_0_write_channel);
 
-  write_output(blur_xy_16_unrolled_1_update_0_write, blur_xy_16_unrolled_1_update_0_write_channel, , size);
+  write_output(blur_xy_16_unrolled_1_update_0_write, blur_xy_16_unrolled_1_update_0_write_channel, size);
 }
 
 }
