d0c8f1fbb020d04d3649c2c5ef16f3c1b8c6cbcc
Cloud User <centos@ip-172-31-31-20.ec2.internal>
2020-04-25 18:26:43 +0000

    changing to uint in hw_uint helped

--- runtime/test_utils.h
+++ runtime/test_utils.h
@@ -41,6 +41,30 @@ void fill_array(const string& in_name,
 
 template<int bits_per_pixel>
 static
+void write_results_decimal(const std::string& out_name,
+    ap_uint<BURST_WIDTH>* y_res,
+    const int nrows,
+    const int ncols,
+    const int transfer_cols) {
+
+  ofstream out(out_name);
+  for (int r = 0; r < nrows; r++) {
+    for (int c = 0; c < transfer_cols; c++) {
+
+      auto offset = r*transfer_cols + c;
+
+      ap_uint<BURST_WIDTH>* val = &(y_res[offset]);
+      for (int l = 0; l < (BURST_WIDTH / bits_per_pixel); l++) {
+        ap_uint<bits_per_pixel> v = (*val)(l*bits_per_pixel + bits_per_pixel - 1, l*bits_per_pixel);
+        out << v << endl;
+      }
+    }
+  }
+  out.close();
+
+}
+template<int bits_per_pixel>
+static
 void write_results(const std::string& out_name,
     ap_uint<BURST_WIDTH>* y_res,
     const int nrows,
