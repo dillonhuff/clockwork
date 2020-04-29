c5f2727df1c71679337fbfeeb76a2bb06e5148cd
Dillon Huff <dillonhuff@gmail.com>
2020-04-29 09:37:43 -0700

    more aws compare updates

--- soda_codes/bxy_ur_8_opt/aws_compare_regressions.sh
+++ soda_codes/bxy_ur_8_opt/aws_compare_regressions.sh
@@ -6,4 +6,4 @@ cd our_code
 ./aws_run_tb_${app}.sh || { echo 'our compilation failed'; exit 1; }
 cd ..
 cd ../../
-../../aligner ./soda_codes/${app}/our_code/regression_result_${app}.txt ./soda_codes/${app}/soda_code/soda_${app}_regression_result.csv
+../../aligner ./soda_codes/${app}/our_code/${app}_update_0_write_accel_result.csv ./soda_codes/${app}/soda_code/soda_${app}_regression_result.csv
