7c63f7fe5dcf309c3f374f1af1089624a2d55242
Cloud User <centos@ip-172-31-62-87.ec2.internal>
2020-05-06 17:56:58 +0000

    generatlized run blur afi script

--- aws_collateral/run_blur_afi.sh
+++ aws_collateral/run_blur_afi.sh
@@ -1,6 +1,11 @@
+awsxclbin_file=$1
+target_afi=$2
+
 sudo fpga-describe-local-image-slots -H
 sudo fpga-clear-local-image -S 0 -H
-sudo fpga-load-local-image -S 0 -I agfi-0749e24d4a285374b -H
+sudo fpga-load-local-image -S 0 -I ${target_afi} -H
+#sudo fpga-load-local-image -S 0 -I agfi-0749e24d4a285374b -H
 
-./host bxy_ur_16_opt.awsxclbin
+./host ${awsxclbin_file}
+#bxy_ur_16_opt.awsxclbin
 
