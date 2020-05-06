79a1035f4908a23d3c6f05faaf479d7bf5b9ac61
Cloud User <centos@ip-172-31-20-135.ec2.internal>
2020-05-06 22:09:15 +0000

    added save soda

---
+++ aws_collateral/save_soda.sh
@@ -0,0 +1,16 @@
+build_dir=$1
+target=$2
+
+/home/centos/aws-fpga/Vitis/tools/create_vitis_afi.sh -xclbin=${build_dir}/${target}.xclbin -o=${target} -s3_bucket=iccad-2020-bucket -s3_dcp_key=dcp_folder -s3_logs_key=logs_folder
+
+aws s3 mb s3://iccad-2020-bucket/app-results/${app}/soda_code/
+
+aws s3 sync . s3://iccad-2020-bucket/app-results/${app}/soda_code/ --exclude '*' --include *.rpt
+aws s3 cp host s3://iccad-2020-bucket/app-results/${app}/soda_code/
+aws s3 cp ${build_dir}/${target}.xclbin.info s3://iccad-2020-bucket/app-results/${app}/soda_code/
+aws s3 cp ${build_dir}/${target}.xclbin s3://iccad-2020-bucket/app-results/${app}/soda_code/
+aws s3 cp ${target}.awsxclbin s3://iccad-2020-bucket/app-results/${app}/soda_code/
+
+
+
+
