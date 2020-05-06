3a90378245026356138c8975adf2db2fd029ebc1
Cloud User <centos@ip-172-31-20-135.ec2.internal>
2020-05-06 22:17:09 +0000

    report

--- aws_collateral/soda_build_vitis.sh
+++ aws_collateral/soda_build_vitis.sh
@@ -1,8 +1,8 @@
 platform=/home/centos/aws-fpga/Vitis/aws_platform/xilinx_aws-vu9p-f1_shell-v04261818_201920_1/xilinx_aws-vu9p-f1_shell-v04261818_201920_1.xpfm
 
-v++ -t hw --platform ${platform} --save-temps --report_level 2 --report_dir ./${app}.rpt --log_dir ./${app}.log -g --temp_dir ./_x.hw.${app} -c -k ${app}_kernel -o ${app}_kernel.xo ${app}_kernel.cpp || { echo 'xo compilation failed'; exit 1; }
+v++ -t hw --platform ${platform} --save-temps --report_level 2 --report_dir ./${app}.report --log_dir ./${app}.log -g --temp_dir ./_x.hw.${app} -c -k ${app}_kernel -o ${app}_kernel.xo ${app}_kernel.cpp || { echo 'xo compilation failed'; exit 1; }
 
-v++ -t hw --platform ${platform} --save-temps --report_level 2 --report_dir ./${app}_pnr.rpt --log_dir ./${app}_pnr.log -g --temp_dir ./${app}_pnr.tmp -k ${app}_kernel -o ${app}_kernel.xclbin -l ${app}_kernel.xo 
+v++ -t hw --platform ${platform} --save-temps --report_level 2 --report_dir ./${app}_pnr.report --log_dir ./${app}_pnr.log -g --temp_dir ./${app}_pnr.tmp -k ${app}_kernel -o ${app}_kernel.xclbin -l ${app}_kernel.xo 
 
 
 
