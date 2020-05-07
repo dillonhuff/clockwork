0d431a5544fabb4c9ca6ae480a60124ef49a1555
Cloud User <centos@ip-172-31-29-118.ec2.internal>
2020-05-07 16:31:34 +0000

    added cp ours

---
+++ aws_collateral/cp_ours_to_aws.sh
@@ -0,0 +1,14 @@
+build_dir=$1
+target=$2
+
+aws s3 mb s3://iccad-2020-bucket/app-results/${app}/our_code/
+
+aws s3 sync . s3://iccad-2020-bucket/app-results/${app}/our_code/ --exclude '*' --include ${build_dir}/${app}_pnr*.rpt
+aws s3 cp host s3://iccad-2020-bucket/app-results/${app}/our_code/
+aws s3 cp ${build_dir}/${target}.xclbin.info s3://iccad-2020-bucket/app-results/${app}/our_code/
+aws s3 cp ${build_dir}/${target}.xclbin s3://iccad-2020-bucket/app-results/${app}/our_code/
+aws s3 cp ${target}.awsxclbin s3://iccad-2020-bucket/app-results/${app}/our_code/
+
+
+
+
