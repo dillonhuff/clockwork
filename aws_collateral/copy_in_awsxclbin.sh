41f9f2ec11feef583a114b84ef95e46cff21cfcb
Cloud User <centos@ip-172-31-18-104.ec2.internal>
2020-05-07 04:06:17 +0000

    updated copy awsxclbin

--- aws_collateral/copy_in_awsxclbin.sh
+++ aws_collateral/copy_in_awsxclbin.sh
@@ -1,2 +1,3 @@
-aws s3 cp s3://iccad-2020-bucket/misc/pw_ur_16_opt.xclbin .
-aws s3 cp s3://iccad-2020-bucket/misc/pw_ur_16_opt.awsxclbin .
+target=$1
+aws s3 cp s3://iccad-2020-bucket/misc/${target}.xclbin .
+aws s3 cp s3://iccad-2020-bucket/misc/${target}.awsxclbin .
