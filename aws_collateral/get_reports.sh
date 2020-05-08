2acb7356fb240c7b2f6d092a05ded730b7619e1e
Cloud User <centos@ip-172-31-19-227.ec2.internal>
2020-05-08 19:25:16 +0000

    added vivado log

--- aws_collateral/get_reports.sh
+++ aws_collateral/get_reports.sh
@@ -1,3 +1,6 @@
 app_name=$1
 aws s3 cp s3://iccad-2020-bucket/app-results/${app_name} . --recursive --exclude "*" --include "*kernel_util_routed.rpt"
+aws s3 cp s3://iccad-2020-bucket/app-results/${app_name} . --recursive --exclude "*" --include "*.xclbin.info"
+
+
 
