75f1ff7d4b65fb961ccff045f215cf5c988e960a
Cloud User <centos@ip-172-31-19-227.ec2.internal>
2020-05-08 04:33:24 +0000

    added get report scripts

--- aws_collateral/get_reports.sh
+++ aws_collateral/get_reports.sh
@@ -1,3 +1,3 @@
-app=$1
-aws s3 cp s3://iccad-2020-bucket/app-results/${app}/ . --exclude "*" --include "*.rpt"
+app_name=$1
+aws s3 cp s3://iccad-2020-bucket/app-results/${app_name} . --recursive --exclude "*" --include "*kernel_util_routed.rpt"
 
