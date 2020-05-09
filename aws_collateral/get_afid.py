004cc13a2faa516bdbae09534cd3178bfbf7fae7
Cloud User <centos@ip-172-31-62-87.ec2.internal>
2020-05-09 23:13:20 +0000

    added script to get afid

--- aws_collateral/get_afid.py
+++ aws_collateral/get_afid.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 
+import json
+import os
+
 print('Getting afid')
+os.system('aws ec2 describe-fpga-images >& fpgas.txt')
+fpgas = open('fpgas.txt').read()
+fpga_strings = json.loads(fpgas)
+
+# print(fpga_strings)
+# print('FPGAs length =', len(fpga_strings))
+
+for image in fpga_strings["FpgaImages"]:
+  if "Name" in image and image["Name"] == "sbl_16_opt":
+    print(image["FpgaImageGlobalId"])
+    break
+
+
