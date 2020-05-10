144cad6ea81c46964414133888bb7d299f0252ef
Cloud User <centos@ip-172-31-62-87.ec2.internal>
2020-05-10 17:06:17 +0000

    updated get afid

--- aws_collateral/get_afid.py
+++ aws_collateral/get_afid.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import os
+import sys 
+  
+# total arguments 
+n = len(sys.argv) 
+print("Total arguments passed:", n) 
+assert(n == 2)
+
+target_image = sys.argv[1]
+  
+print("\nName of Python script:", sys.argv[0]) 
+print('target image =', target_image)
 
 print('Getting afid')
 os.system('aws ec2 describe-fpga-images >& fpgas.txt')
@@ -12,7 +23,7 @@ fpga_strings = json.loads(fpgas)
 # print('FPGAs length =', len(fpga_strings))
 
 for image in fpga_strings["FpgaImages"]:
-  if "Name" in image and image["Name"] == "sbl_16_opt":
+  if "Name" in image and image["Name"] == target_image:
     print(image["FpgaImageGlobalId"])
     break
 
