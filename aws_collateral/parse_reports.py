44267a14332ed251905032aebfc3dfd7b03c82c5
Cloud User <centos@ip-172-31-21-29.ec2.internal>
2020-05-11 19:04:28 +0000

    added more utils to report parser

--- aws_collateral/parse_reports.py
+++ aws_collateral/parse_reports.py
@@ -13,7 +13,14 @@ assert(n == 2)
 app = sys.argv[1]
 
 print('app =', app)
+import glob
 
 os.system('../aws_collateral/get_reports.sh {0}'.format(app))
+directory = './'
+for filename in glob.iglob(directory + '**/*', recursive=True):
+  if filename.endswith("kernel_util_routed.rpt"):
+    print('File:', filename)
+    util = open(filename, 'r').read()
+    print('Util', util)
+    break
 
-# TODO: Clear reports
