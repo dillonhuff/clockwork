0249780c1c37e75724dddd1de47634471b7453ce
Kavya Sreedhar <skavya@rice10.stanford.edu>
2020-05-15 15:33:16 -0700

    skavya settings

---
+++ user_settings/skavya_settings.sh
@@ -0,0 +1,6 @@
+export BARVINOK_PATH=/home/skavya/barvinok-0.41/isl/
+export ISL_PATH=/home/skavya/barvinok-0.41/isl/
+export OPT_PATH=/home/skavya/include
+export OPT_LIB_PATH=/home/skavya/lib
+
+export CXX=g++
