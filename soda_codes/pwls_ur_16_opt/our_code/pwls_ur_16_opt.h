289a6109c1e6c13d82f4ac41d098695029d8278f
Cloud User <centos@ip-172-31-62-87.ec2.internal>
2020-05-18 15:24:05 +0000

    changes to dn

--- soda_codes/pwls_ur_16_opt/our_code/pwls_ur_16_opt.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "hw_classes.h"
-
-void pwls_ur_16_opt(HWStream<hw_uint<256> >& /* get_args num ports = 16 */input_arg, HWStream<hw_uint<256> >& /* get_args num ports = 16 */pwls_ur_16);
