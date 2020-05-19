da764c59b2c56a05d398804a15069a06e10fdf63
Dillon Huff <dillonhuff@gmail.com>
2020-05-19 11:20:24 -0700

    updated to include the new run image scripts

--- aws_collateral/run_soda_image.sh
+++ aws_collateral/run_soda_image.sh
@@ -1,2 +1,7 @@
-../../../aws_collateral/rebuild_host.sh soda_${app}_host.cpp
+afid=$(../../../aws_collateral/get_afid.py ${app}_kernel 2>&1)
+echo 'afid ='$afid
+../../../aws_collateral/rebuild_host.sh soda_${app}_host.cpp || { echo 'failed to build host'; exit 1; }
+../../../aws_collateral/load_soda_awsxclbin.sh || { ehco 'failed to load awsxclbin'; exit 1; }
+../../../aws_collateral/run_blur_afi.sh ${app}_kernel.awsxclbin ${afid}
+echo 'done'
 
