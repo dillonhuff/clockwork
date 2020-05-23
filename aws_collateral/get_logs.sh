05e8f66097d1b614434f9160d6fd44efe72c976c
Cloud User <centos@ip-172-31-21-29.ec2.internal>
2020-05-23 05:23:14 +0000

    adding log script

---
+++ aws_collateral/get_logs.sh
@@ -0,0 +1,5 @@
+app_name=$1
+aws s3 cp s3://iccad-2020-bucket/app-results/${app_name} . --recursive --exclude "*" --include "*.log"
+
+
+
---
+++ aws_collateral/parse_runtime.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys 
+
+import os
+
+n = len(sys.argv) 
+print("Total arguments passed:", n) 
+assert(n == 2)
+
+app = sys.argv[1]
+
+print('app =', app)
+import glob
+
+os.system('rm -rf our_code')
+os.system('rm -rf soda_code')
+os.system('../aws_collateral/get_logs.sh {0}'.format(app))
+directory = './'
+for filename in glob.iglob(directory + '**/*', recursive=True):
+  if filename.endswith(".log"):
+    print('File:', filename)
+    # util = open(filename, 'r').read()
+    # latex_table = util.replace("%", "\\%")
+    # latex_table = latex_table.replace("|", "&")
+    # latex_table = latex_table.replace("[ ", "[")
+    # latex_table = latex_table.replace("_", "u")
+    # print(latex_table)
+    # break
+
