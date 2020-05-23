ff591460547fdb165b8743814bfffbd8bc34edf8
Cloud User <centos@ip-172-31-21-29.ec2.internal>
2020-05-23 05:36:45 +0000

    creating regex

--- aws_collateral/parse_runtime.py
+++ aws_collateral/parse_runtime.py
@@ -3,7 +3,7 @@
 import json
 import os
 import sys 
-
+import re
 import os
 
 n = len(sys.argv) 
@@ -15,14 +15,24 @@ app = sys.argv[1]
 print('app =', app)
 import glob
 
+vpp_log_name = "v++_{0}.log".format(app)
+
 os.system('rm -rf our_code')
 os.system('rm -rf soda_code')
 os.system('../aws_collateral/get_logs.sh {0}'.format(app))
 directory = './'
 for filename in glob.iglob(directory + '**/*', recursive=True):
-  if filename.endswith(".log"):
+  if filename.endswith(vpp_log_name):
     print('File:', filename)
-    # util = open(filename, 'r').read()
+    lines = open(filename, 'r').readlines()
+    for l in lines:
+      target = "^INFO\: \[v\+\+ 60-791\]"
+      #"INFO: [v++ 60-791] Total elapsed time:" # (\d+)h (\d+)m (\d+)s"
+      x = re.search(target, l) 
+      if x:
+        print(l)
+
+
     # latex_table = util.replace("%", "\\%")
     # latex_table = latex_table.replace("|", "&")
     # latex_table = latex_table.replace("[ ", "[")
