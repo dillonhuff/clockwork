3d817169ad9e7985f059e9eb60859f6891880e29
Dillon Huff <dillonhuff@gmail.com>
2020-05-25 00:48:43 -0700

    adding sliced images

--- image_slicer.cpp
+++ image_slicer.cpp
@@ -6,9 +6,14 @@ using namespace std;
 int main() {
 
   bitmap_image image("./images/taxi.bmp");
+  
+  //int rows = 128;
+  //int cols = 128;
 
-  int rows = 128;
-  int cols = 128;
+  int size = 128;
+
+  int rows = size;
+  int cols = size;
 
   int row_start = 500;
   int col_start = 500;
@@ -16,13 +21,13 @@ int main() {
   bitmap_image smaller(cols, rows);
   for (int r = row_start; r < row_start + rows; r++) {
     for (int c = col_start; c < col_start + cols; c++) {
-      cout << "r, c = " << r << ", " << c << endl;
+      //cout << "r, c = " << r << ", " << c << endl;
       rgb_t pix;
       image.get_pixel(c, r, pix);
-      cout << "setting pixels" << endl;
+      //cout << "setting pixels" << endl;
       smaller.set_pixel(c - col_start, r - row_start, pix);
     }
   }
 
-  smaller.save_image("taxi_slice.bmp");
+  smaller.save_image("taxi_slice_" + to_string(size) + ".bmp");
 }
