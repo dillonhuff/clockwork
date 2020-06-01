5851f49b9df8d04860e7546499ab111d9b9d9464
Dillon Huff <dillonhuff@gmail.com>
2020-05-31 20:32:02 -0700

    adding pytyhon

--- install_deps_rice.sh
+++ install_deps_rice.sh
@@ -4,7 +4,6 @@ wget https://shoup.net/ntl/ntl-11.4.1.tar.gz
 tar -xvzf ntl-11.4.1.tar.gz
 cd ntl-11.4.1/src
 ./configure NTL_GMP_LIP=on PREFIX=$top_dir
-#./configure NTL_GMP_LIP=on PREFIX=/home/dhuff/clockwork/ GMP_PREFIX=/usr/lib/x86_64-linux-gnu/
 make
 make install
 cd ../../
@@ -12,7 +11,6 @@ cd ../../
 curl -OL barvinok.gforge.inria.fr/barvinok-0.41.tar.gz
 tar -xvzf barvinok-0.41.tar.gz
 cd barvinok-0.41/
-#./configure --prefix=$top_dir --with-gmp-prefix=/usr/lib/x86_64-linux-gnu/ --with-ntl-prefix=$top_dir
 ./configure --prefix=$top_dir --with-ntl-prefix=$top_dir
 make
 make install
