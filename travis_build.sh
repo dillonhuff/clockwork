d4fb4cfd99a8d7ea28566d3d2fff49ee7fbdc8a9
Dillon Huff <dillonhuff@gmail.com>
2020-05-31 22:30:39 -0700

    path printout

--- travis_build.sh
+++ travis_build.sh
@@ -2,6 +2,7 @@ source ./user_settings/travis_settings.sh
 ./make_code.sh
 if [ $? -eq 0 ]; then
   echo "Cmd: " $1
+  echo "LD_LIBRARY_PATH:"$LD_LIBRARY_PATH
   ./clockwork $1
 else
   echo "Build FAILED"
