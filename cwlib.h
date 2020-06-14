356d3f9fafaa8abab37b5ed069f33284edc15a36
joyliu37 <joyliu37@sina.com>
2020-06-14 14:30:16 -0700

    add tracked new file

---
+++ cwlib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#include "coreir/common-macros.h"
+#include "coreir-c/ctypes.h"
+
+#ifdef __cplusplus
+#include "coreir.h"
+COREIR_GEN_CPP_API_DECLARATION_FOR_LIBRARY(cwlib);
+#endif
+
+COREIR_GEN_C_API_DECLARATION_FOR_LIBRARY(cwlib);
