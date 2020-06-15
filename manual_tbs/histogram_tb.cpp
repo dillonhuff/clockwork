6a4119f50749b5dbf4a51fd3148d455c25fed057
Dillon Huff <dillonhuff@gmail.com>
2020-06-15 14:14:45 -0700

    counts match expected

--- manual_tbs/histogram_tb.cpp
+++ manual_tbs/histogram_tb.cpp
@@ -13,6 +13,17 @@ int main() {
   HWStream<hw_uint<32> > color_counts;
 
   vector<hw_uint<32> > values{1, 1, 4, 7, 1, 3, 4, 8, 11, 8, 3, 2, 4, 5, 1, 0, 4, 14, 3, 5};
+  vector<hw_uint<32> > expected;
+  for (int i = 0; i < (int) values.size(); i++) {
+    expected.push_back(0);
+  }
+  cout << "set expected" << endl;
+  for (int i = 0; i < 20; i++) {
+    auto v = values.at(i);
+    expected[v.to_int()] = expected[v.to_int()] + 1;
+  }
+
+  cout << "writing values" << endl;
   for (auto v : values) {
     off_chip_img.write(v);
   }
@@ -21,7 +32,9 @@ int main() {
 
   cout << "Done with histogram" << endl;
   for (int i = 0; i < 20; i++) {
-    cout << "count " << i << " = " << color_counts.read() << endl;
+    auto cnt = color_counts.read();
+    cout << "count " << i << " = " << cnt << endl;
+    assert(cnt == expected.at(i));
   }
 
   return 0;
