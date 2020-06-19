d24eb6908d3e17d03b06dcf3f887a1cbb180dd8b
Dillon Huff <dillonhuff@gmail.com>
2020-06-18 19:00:31 -0700

    added kiwi setup

---
+++ misc/kiwi_setup.sh
@@ -0,0 +1,6 @@
+cp ./user_settings/generic_linux_settings.sh ./user_settings/private_settings.sh
+source ./user_settings/private_settings.sh
+
+source ./misc/install_deps_linux.sh
+
+git clone https://github.com/rdaly525/coreir.git
