8cbf73a20b6ad7aed157720b9344484a66dc6a38
Cloud User <centos@ip-172-31-68-79.ec2.internal>
2020-06-25 22:19:22 +0000

    adding aws setup build

---
+++ misc/aws_setup.sh
@@ -0,0 +1,7 @@
+cp ./user_settings/linux_settings_template.sh ./user_settings/private_settings.sh
+yum install gmp
+source ./user_settings/private_settings.sh
+
+source ./misc/install_deps_linux.sh
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/lib
