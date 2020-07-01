42991100760fc94c5f68e877698f24034f02db27
Dillon Huff <dillonhuff@gmail.com>
2020-07-01 13:00:10 -0700

    removed blur example

--- blur_example/blur_example_opt.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "hw_classes.h"
-
-void blur_example_opt(HWStream<hw_uint<32> >& /* get_args num ports = 2 */input_arg, HWStream<hw_uint<32> >& /* get_args num ports = 2 */blur_example);
