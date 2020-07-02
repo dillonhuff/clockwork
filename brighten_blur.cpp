b312d8257391139ca6ddb9ed00d20d4d860604df
Isabela Rodrigues <isabdr@stanford.edu>
2020-07-02 17:02:06 -0300

    Hello cat

--- brighten_blur.cpp
+++ brighten_blur.cpp
@@ -5,6 +5,7 @@ using namespace std;
   // Debug utility
   ofstream* global_debug_handle;
 
+//Hello I am a cat
 #endif //__VIVADO_SYNTH__
 // compute file: clockwork_standard_compute_units.h
 #include "clockwork_standard_compute_units.h"
