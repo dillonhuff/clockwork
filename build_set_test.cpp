62611222ae8cace5dd85b02c00d9368047296e20
Dillon Huff <dillonhuff@gmail.com>
2020-07-13 21:20:14 -0700

    added ps 16

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -12040,7 +12040,7 @@ pair<string, string> store_off_chip_two_channels(const std::string& input, App& 
 void psef_multi_output_test() {
   int rows = 1080;
   int cols = 1920 / 2;
-  int unroll = 1;
+  int unroll = 16;
 
   App lp;
   lp.set_default_pixel_width(16);
