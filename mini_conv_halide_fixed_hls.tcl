66e5ddf1ec6fc3771ab6e1c49a558c5c247f0550
Isabela Barros David Rodrigues <isabdr@caddy01.stanford.edu>
2020-07-15 10:38:57 -0700

    Committing everything from caddy

--- mini_conv_halide_fixed_hls.tcl
+++ mini_conv_halide_fixed_hls.tcl
@@ -1,8 +1,8 @@
-open_project -reset mini_conv_halide_fixed_proj
-set_top mini_conv_halide_fixed
-add_files -cflags "-I ../../ -std=c++11 -D__VIVADO_SYNTH__" mini_conv_halide_fixed.cpp
-add_files -cflags "-I ../../ -std=c++11 -D__VIVADO_SYNTH__" mini_conv_halide_fixed.cpp
-add_files -cflags "-I ../.. -std=c++11 -D__VIVADO_SYNTH__" -tb tb_soda_mini_conv_halide_fixed.cpp
+open_project -reset unoptimized_mini_conv_halide_fixed_proj
+set_top unoptimized_mini_conv_halide_fixed
+add_files -cflags "-I ../../ -std=c++11 -D__VIVADO_SYNTH__" unoptimized_mini_conv_halide_fixed.cpp
+add_files -cflags "-I ../../ -std=c++11 -D__VIVADO_SYNTH__" unoptimized_mini_conv_halide_fixed.cpp
+add_files -cflags "-I ../.. -std=c++11 -D__VIVADO_SYNTH__" -tb tb_soda_unoptimized_mini_conv_halide_fixed.cpp
 open_solution -reset "solution1"
 set_part {xc7k160tfbg484-2}
 list_core
