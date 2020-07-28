af5a740652b3dd4ea7324b3d8892da6109916251
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-07-28 16:49:37 -0700

    comma and [] works except for double digit numbers

--- lake_stream/identity_stream/script.py
+++ lake_stream/identity_stream/script.py
@@ -12,6 +12,33 @@ def zero_arr(width):
     return data0
 
 
+def format_comma_bracket(string):
+    l = list(string)
+    newl = list(string)
+    ni = 0
+    for i in range(len(l)):
+        char = l[i]
+        if i == 1:
+            ni = 1
+        try:
+            if isinstance(int(char), int):
+                newl.insert(ni, '[')
+                if l[i + 1] == ']':
+                    newl.insert(ni + 2, ']')
+                    ni += 2
+                else:
+                    newl.insert(ni + 2, '],')
+                    ni += 2
+        except ValueError:
+            assert True
+
+        ni += 1
+    new_string = ""
+    for item in newl:
+        new_string += item
+    return new_string
+
+
 def parse(csv_file_name, data_in_width, data_out_width):
     with open(csv_file_name[:-4] + '_parse.csv', 'w') as parsefile:
         filewriter = csv.writer(parsefile, delimiter=',',
@@ -23,24 +50,25 @@ def parse(csv_file_name, data_in_width, data_out_width):
         reader = csv.reader(csv_file, delimiter=',')
 
         # zeros for input / output data when there is no write / read
-        data_in0 = zero_arr(data_in_width)
-        data_out0 = zero_arr(data_out_width)
+        data_in0 = format_comma_bracket(zero_arr(data_in_width))
+        data_out0 = format_comma_bracket(zero_arr(data_out_width))
 
         # TODO clean this up -> issues with initializing array?
         data = []
         for row in reader:
             # there is a read and write on this cycle
+            format_row = format_comma_bracket(row[1][5:])
             if len(data) > 0 and \
                     (int(data[len(data) - 1][0]) == int(row[0][3:])):
                 if row[0][0:2] == "rd":
-                    data[len(data) - 1][2] = row[1][5:]
+                    data[len(data) - 1][2] = format_row
                 else:
-                    data[len(data) - 1][1] = row[1][5:]
+                    data[len(data) - 1][1] = format_row
             else:
                 if row[0][0:2] == "rd":
-                    data.append([row[0][3:], data_in0, row[1][5:]])
+                    data.append([row[0][3:], data_in0, format_row])
                 else:
-                    data.append([row[0][3:], row[1][5:], data_out0])
+                    data.append([row[0][3:], format_row, data_out0])
 
         # add rows with no ops that do not have a cycle count
         i = 0
