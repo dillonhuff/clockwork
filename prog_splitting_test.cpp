8d920150cbddc1126365392f8ea92bb3e41c18d2
Isabela Rodrigues <isabdr@stanford.edu>
2020-07-28 17:08:59 -0300

    Add normalized function

--- prog_splitting_test.cpp
+++ prog_splitting_test.cpp
@@ -7,6 +7,7 @@
 #define INT_ADDER_COST 1
 #define INT_CONSTANT_DIVIDER_COST 1
 
+typedef address normalized_address;
 
 struct TargetTechlibInfo {
   map<string, int> compute_unit_costs;
@@ -44,40 +45,29 @@ map<string, int> estimate_kernel_areas(prog& prg, TargetTechlibInfo& target_info
 	return costs;
 }
 
-
-//------------------------------------------------IS_POINTWISE--------------------------------------------------
-
-bool is_pointwise(const string& buff, prog& prg){
-
+std::set<normalized_address> get_normalized_addresses(const string& buff, prog& prg){
+	
 	std::set<op*> buff_readers = find_readers(buff, prg);
 	op* buff_writer = find_writer(buff, prg);
 	std::set<address> addresses;
 	auto levels = get_variable_levels(prg);
-	std::set<address> normalized_addresses;
+	std::set<normalized_address> normalized_addresses;
 
 	// Add addresses from reader ops
 	for(auto reader : buff_readers){
 		for(auto addr : reader->read_addrs(buff)){
-			if(addr.size() != 1){
-				return false; //what does this mean?
-			}else{
-				addresses.insert(addr.at(0).second);
-			}	
+			addresses.insert(addr.at(0).second);	
 		}
 	}
 
 	// Add addresses from writer op
 	for(auto addr : buff_writer->write_addrs(buff)){
-		if(addr.size() != 1){
-			return false;
-		}else{
-			addresses.insert(addr.at(0).second);
-		}	
+		addresses.insert(addr.at(0).second);	
 	}
 
 	// Change every address' variable name for its <level>
 	for(auto addr : addresses){
-		address normalized = addr;
+		normalized_address normalized = addr;
 		for(auto level : levels){
 			string var = level.first;
 			int level_index = level.second;
@@ -96,14 +86,25 @@ bool is_pointwise(const string& buff, prog& prg){
 	cout << "Normalized addresses in:  " << buff << endl;
 	for(auto addr : normalized_addresses){
 		cout << addr << endl;
-
 	}
 
+	return normalized_addresses;
+}
+
+//------------------------------------------------IS_POINTWISE--------------------------------------------------
+
+bool is_pointwise(const string& buff, prog& prg){
 	// Check if it's pointwise
-	if(normalized_addresses.size() == 1){
+	if(get_normalized_addresses(buff, prg).size() == 1){
 		cout << buff << " is POINTWISE!" << endl;
 		return true;
 	}	
+
+	return false;
+}
+
+
+bool is_stencil(const string& buff, prog& prg){
 
 	return false;
 }
@@ -112,7 +113,31 @@ bool is_pointwise(const string& buff, prog& prg){
 
 int num_locs_written(const string& buff, prog& prg){
 
-	return 0;
+	int kernel_cost = 0;
+	uset* all_locs_written = nullptr;
+	auto ops_to_writes = prg.producer_maps();
+
+	// Loop through all ops in the prog checking whether they are producers and writing on the buff
+	// Update all_locs_written if necessary at each op
+	for (auto op: prg.all_ops()){
+		isl_map* write = map_find(op, ops_to_writes);
+		if(range_name(write)==buff){
+			cout << tab(1) << op -> name << endl;
+			auto locs_written = write;
+			cout << tab(2) << str(locs_written) << endl;
+			auto locs = range(locs_written);
+			cout << tab(2) << str(locs) << endl;
+			if(all_locs_written == nullptr){
+				all_locs_written = to_uset(locs);
+			}else{
+				all_locs_written = unn(to_uset(locs), all_locs_written);
+			}
+		}
+	}
+
+	int num_locs_written = int_upper_bound(card(all_locs_written));
+	cout << tab(2) << "Number of locations written: " << num_locs_written << endl;
+	return num_locs_written;
 }
 
 //-----------------------------------------ESTIMATE_KERNEL_MEMORY_AREA-------------------------------------------
@@ -133,42 +158,19 @@ int num_locs_written(const string& buff, prog& prg){
 
 	 // Check each buff to estimate its size
 	 for(auto buff : buffers){
-		 if(is_pointwise(buff, prg)){
+		 if(prg.is_boundary(buff)){
 			 estimated_buffer_sizes[buff] = 0;
-		 }else{
+		 } else if(is_pointwise(buff, prg)){
+			 estimated_buffer_sizes[buff] = 0;
+		 } else if(is_stencil(buff, prg)){	
+			 estimated_buffer_sizes[buff] = 0; //To be implemented!
+		 } else{
 			 estimated_buffer_sizes[buff] = num_locs_written(buff, prg);
 		 }
 	 }
 
-	 map<string, int> estimated_areas;
-	 auto locations_written = prg.producer_maps();
-
-	// Loop through all kernels
-	for (string kernel : get_kernels(prg)) {
-		op* loop = prg.find_loop(kernel);
-		auto ops_in_kernel = loop -> descendant_ops();
-		cout << "ops_in_kernel " << kernel << ":" << endl;
-		int kernel_cost = 0;
-		uset* all_locs_written = nullptr;
-		// Loop through all ops in the kernel
-		for (auto op: ops_in_kernel){
-			cout << tab(1) << op -> name << endl;
-			auto locs_written = map_find(op, locations_written);
-			cout << tab(2) << str(locs_written) << endl;
-			auto locs = range(locs_written);
-			cout << tab(2) << str(locs) << endl;
-			if(all_locs_written == nullptr){
-				all_locs_written = to_uset(locs);
-			}else{
-				all_locs_written = unn(to_uset(locs), all_locs_written);
-			}
-		}
-
-		int num_locs_written = int_upper_bound(card(all_locs_written));
-		cout << tab(2) << "Number of locations written: " << num_locs_written << endl;
-		estimated_areas[kernel] = num_locs_written;
-		cout << "Kernel " << kernel << " MEMORY  cost: " << estimated_areas[kernel] << endl;
-	}
+	map<string, int> estimated_areas;
+	auto locations_written = prg.producer_maps();
 
 	return estimated_areas;
  }
@@ -267,13 +269,56 @@ void generate_optimized_code_for_program_dag(std::vector<prog>& group_programs) 
   }
 }
 
+
+prog simple_stencil(){
+
+  prog prg;
+  prg.compute_unit_file = "clockwork_standard_compute_units.h";
+  prg.name = "brighten_blur";
+  prg.add_input("off_chip_input");
+  prg.add_output("off_chip_output");
+  prg.buffer_port_widths["off_chip_input"] = 16;
+  prg.buffer_port_widths["in"] = 16;
+  prg.buffer_port_widths["off_chip_output"] = 16;
+
+  int input_image_rows = 256;
+  int input_image_cols = 256;
+
+  prg.buffer_bounds["off_chip_input"] = {input_image_cols, input_image_rows};
+  prg.buffer_bounds["in"] = {input_image_cols, input_image_rows};
+  prg.buffer_bounds["off_chip_output"] = {input_image_cols - 2, input_image_rows - 2};
+
+  auto p = prg.add_nest("po", 0, input_image_rows, "pi", 0, input_image_cols);
+  auto write = p->add_op("load_image_from_off_chip");
+  write->add_load("off_chip_input", "pi, po");
+  write->add_store("in", "pi, po");
+
+  auto write_out = prg.add_nest("m", 0, input_image_rows-2, "n", 0, input_image_cols-2);
+  auto write_op = write_out->add_op("write_blurred_off_chip");
+  write_op->add_load("in", "n, m");
+  write_op->add_load("in", "n, m+1");
+  write_op->add_load("in", "n, m+2");
+  write_op->add_load("in", "n+1, m");
+  write_op->add_load("in", "n+1, m+1");
+  write_op->add_load("in", "n+1, m+2");
+  write_op->add_load("in", "n+2, m");
+  write_op->add_load("in", "n+2, m+1");
+  write_op->add_load("in", "n+2, m+2");
+  write_op->add_store("off_chip_output", "n, m");
+
+  return prg;
+
+}
+
 //--------------------------------------------------TOY_TASK----------------------------------------------------------
 void toy_task(){
 
+
 	vector<prog> example_progs;
+	example_progs.push_back(simple_stencil());
 	example_progs.push_back(brighten_blur());
-	example_progs.push_back(unet_conv_3_3());
-	example_progs.push_back(resnet());
+	//example_progs.push_back(unet_conv_3_3());
+	//example_progs.push_back(resnet());
 
 	vector<pair<string, int>> prog_costs;
 
@@ -323,6 +368,10 @@ void toy_task(){
 			target_info.compute_unit_costs["hcompute_conv_stencil_8"] = 0;
 			target_info.compute_unit_costs["hcompute_conv_stencil_9"] = 0;
 			target_info.compute_unit_costs["hcompute_hw_output_stencil"] = 0;
+		} else if(prg.name == "brighten_blur"){
+			target_info.compute_unit_costs["multiply_by_two"] = INT_MULTIPLIER_COST;
+			target_info.compute_unit_costs["blur_3_3"] = INT_ADDER_COST*8 + INT_CONSTANT_DIVIDER_COST;
+			target_info.compute_unit_costs["inc"] = INT_ADDER_COST*8 + INT_CONSTANT_DIVIDER_COST;
 		}
 
 		estimate_kernel_memory_area(prg, target_info);
@@ -334,7 +383,7 @@ void toy_task(){
 			total_cost += estimate_area.second;
 			//		cout << "Each cost: " << estimate_area.second << endl;
 		}
-		/*
+		
 		   cout << endl << "Grouping kernels..." << endl;
 		   int max_area_cost_per_group = 9;
 		   std::set<std::set<string>> kernel_grouping = group_kernels_for_compilation(prg, kernel_areas, max_area_cost_per_group);
@@ -355,7 +404,7 @@ void toy_task(){
 		prog_for_group.pretty_print();
 		group_programs.push_back(prog_for_group);
 		}
-		 */
+		 
 		pair<string, int> result;
 		result.first = prg.name;
 		result.second = total_cost; 
