92290f0bb0977b335e8f985253816c7c641127db
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-07-28 14:58:09 -0700

    pycodestyle

--- lake_stream/identity_stream/script.py
+++ lake_stream/identity_stream/script.py
@@ -1,4 +1,8 @@
+# this file parses outputs from Clockwork for the
+# specified input format for the SMT solver
+
 import csv
+
 
 def zero_arr(width):
     data0 = '['
@@ -6,6 +10,7 @@ def zero_arr(width):
         data0 += '0 '
     data0 = data0[0:-1] + ']'
     return data0
+
 
 def parse(csv_file_name, data_in_width, data_out_width):
     with open(csv_file_name[:-4] + '_parse.csv', 'w') as parsefile:
@@ -20,12 +25,13 @@ def parse(csv_file_name, data_in_width, data_out_width):
         # zeros for input / output data when there is no write / read
         data_in0 = zero_arr(data_in_width)
         data_out0 = zero_arr(data_out_width)
-        
+
         # TODO clean this up -> issues with initializing array?
         data = []
         for row in reader:
             # there is a read and write on this cycle
-            if len(data) > 0 and (int(data[len(data) - 1][0]) == int(row[0][3:])):
+            if len(data) > 0 and \
+                    (int(data[len(data) - 1][0]) == int(row[0][3:])):
                 if row[0][0:2] == "rd":
                     data[len(data) - 1][2] = row[1][5:]
                 else:
@@ -35,7 +41,7 @@ def parse(csv_file_name, data_in_width, data_out_width):
                     data.append([row[0][3:], data_in0, row[1][5:]])
                 else:
                     data.append([row[0][3:], row[1][5:], data_out0])
- 
+
         # add rows with no ops that do not have a cycle count
         i = 0
         prev_dat = -1
@@ -50,7 +56,8 @@ def parse(csv_file_name, data_in_width, data_out_width):
                 prev_dat += 1
                 filewriter.writerow([str(prev_dat), data_in0, data_out0])
 
-parse('buf_agg_SMT.csv', 1, 4)
-parse('buf_sram_SMT.csv', 4, 4)
-parse('buf_tb_SMT.csv', 4, 1)
 
+if __name__ == "__main__":
+    parse('buf_agg_SMT.csv', 1, 4)
+    parse('buf_sram_SMT.csv', 4, 4)
+    parse('buf_tb_SMT.csv', 4, 1)
