81894b4c976dd2b0ff62ba73eccc26ad3cf7f39e
Dillon Huff <dillonhuff@gmail.com>
2020-07-30 21:34:55 -0600

    unroll with generated tb passes basic tests

--- prog.cpp
+++ prog.cpp
@@ -2732,16 +2732,19 @@ void generate_regression_testbench(prog& prg) {
   rgtb << "int main() {" << endl;
   rgtb << tab(1) << "ofstream fout(\"" << "regression_result_" << prg.name << ".txt\");" << endl;
 
-  //vector<string> unoptimized_streams;
   vector<string> optimized_streams;
+  map<string, int> unroll_factor;
   for (auto in : prg.ins) {
     auto readers = find_readers(in, prg);
     int width = 0;
+    int unroll = 0;
     for (auto reader : readers) {
       for (auto addr : reader->read_addrs(in)) {
         width += prg.buffer_port_width(in);
+        unroll++;
       }
     }
+    unroll_factor[in] = unroll;
     rgtb << tab(1) << "HWStream<hw_uint<" << width << " > > " << in << ";" << endl;
     //rgtb << tab(1) << "HWStream<" << prg.buffer_element_type_string(in) << " > " << in << ";" << endl;
     optimized_streams.push_back(in);
@@ -2750,11 +2753,14 @@ void generate_regression_testbench(prog& prg) {
   for (auto out : prg.outs) {
     auto readers = find_writers(out, prg);
     int width = 0;
+    int unroll = 0;
     for (auto reader : readers) {
       for (auto addr : reader->write_addrs(out)) {
         width += prg.buffer_port_width(out);
+        unroll++;
       }
     }
+    unroll_factor[out] = unroll;
     rgtb << tab(1) << "HWStream<hw_uint<" << width << " > > " << out << ";" << endl;
     //rgtb << tab(1) << "HWStream<" << prg.buffer_element_type_string(out) << " > " << out << ";" << endl;
     optimized_streams.push_back(out);
@@ -2769,13 +2775,22 @@ void generate_regression_testbench(prog& prg) {
     auto rng = range(read_map);
     auto range_card = card(rng);
     int num_pushes = int_upper_bound(range_card);
+    int unroll = map_find(in, unroll_factor);
+    int lane_width = prg.buffer_port_width(in);
+    int bundle_width = lane_width*unroll;
 
     rgtb << tab(1) << "// cmap    : " << str(cmap) << endl;
     rgtb << tab(1) << "// read map: " << str(read_map) << endl;
     rgtb << tab(1) << "// rng     : " << str(rng) << endl;
     rgtb << tab(1) << "// rng card: " << str(range_card) << endl;
-    rgtb << tab(1) << "for (int i = 0; i < " << num_pushes << "; i++) {" << endl;
-    rgtb << tab(2) << in << ".write(i);" << endl;
+    int num_transfers = num_pushes;
+    rgtb << tab(1) << "for (int i = 0; i < " << num_transfers << "; i++) {" << endl;
+    vector<string> inds;
+    for (int i = 0; i < unroll; i++) {
+      inds.push_back(str(unroll) + "*i + " + str(i));
+    }
+    pack_bv(2, rgtb, "value", inds, lane_width);
+    rgtb << tab(2) << in << ".write(value);" << endl;
     rgtb << tab(1) << "}" << endl << endl;
   }
   rgtb << tab(1) << prg.name << "(" << comma_list(optimized_streams) << ");" << endl;
@@ -2786,9 +2801,17 @@ void generate_regression_testbench(prog& prg) {
     auto rng = range(read_map);
     auto range_card = card(rng);
     int num_pops = int_upper_bound(range_card);
+    int unroll = map_find(out, unroll_factor);
+    int lane_width = prg.buffer_port_width(out);
+    int bundle_width = lane_width*unroll;
+
     rgtb << tab(1) << "for (int i = 0; i < " << num_pops << "; i++) {" << endl;
-    rgtb << tab(2) << "int actual = " << out << ".read();" << endl;
-    rgtb << tab(2) << "fout << actual << endl;" << endl;
+    rgtb << tab(2) << "auto actual = " << out << ".read();" << endl;
+    vector<string> results = split_bv(2, rgtb, "actual", lane_width, unroll);
+    for (auto r : results) {
+      rgtb << tab(2) << "fout << " << r << " << endl;" << endl;
+    }
+    //rgtb << tab(2) << "fout << actual << endl;" << endl;
     rgtb << tab(1) << "}" << endl << endl;
   }
   rgtb << tab(1) << "return 0;" << endl;
