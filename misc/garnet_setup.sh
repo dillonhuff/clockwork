02cd609eeb5b6ce09b0ae6ec31ead1bc3de210df
Dillon Huff <dillonhuff@gmail.com>
2020-08-07 14:18:26 -0700

    updated config

--- misc/garnet_setup.sh
+++ misc/garnet_setup.sh
@@ -5,6 +5,7 @@ apt-get install -y tmux
 git clone https://github.com/dillonhuff/VimSetup.git
 cp ./VimSetup/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # to build:
 # dont forget to change memory_core_magma to return [] for configs
