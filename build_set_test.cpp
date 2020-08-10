ee61cf5e5490690d7b8a80c3f4b88dee7640131d
Dillon Huff <dillonhuff@gmail.com>
2020-08-09 17:00:59 -0700

    checking for misaligned loops

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -12851,11 +12851,55 @@ void adjust_inner_iis(schedule_info& sched, prog& prg) {
   }
 }
 
+bool is_perfect(op* loop, prog& prg) {
+  assert(loop->is_loop);
+  if (is_inner_loop(loop)) {
+    return true;
+  }
+
+  if (loop->children.size() > 1) {
+    return false;
+  }
+
+  return is_perfect(loop->children.at(0), prg);
+}
+
+bool all_perfect_loop_nests(prog& prg) {
+  for (auto l : prg.all_loops()) {
+    if (l->name != "root") {
+      if (!is_perfect(l, prg)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool all_loop_nests_same_depth(prog& prg) {
+  auto ops = prg.all_ops();
+
+  if (ops.size() == 0) {
+    return true;
+  }
+  std::set<int> depths;
+  for (auto op : ops) {
+    depths.insert(surrounding_vars(op, prg).size());
+  }
+  return depths.size() == 1;
+}
+
 void garnet_dual_port_ram_schedule(schedule_info& sched, op* root, prog& prg) {
   auto rvars = reduce_vars(prg);
-  if (rvars.size() == 0) {
+  bool perfect = all_perfect_loop_nests(prg);
+  if (rvars.size() == 0 &&
+      perfect) {
+    bool single_depth = all_loop_nests_same_depth(prg);
+
+    assert(single_depth);
+
     prg.pretty_print();
     cout << prg.name << " is a stencil pipeline" << endl;
+    assert(false);
     auto valid = prg.validity_deps();
     auto dom = prg.whole_iteration_domain();
     umap* clksched_map = clockwork_schedule_umap(dom, valid, cpy(valid));
@@ -13154,7 +13198,7 @@ void test_schedules(vector<prog>& test_programs) {
     for (auto m : get_maps(ss)) {
       cout << tab(1) << str(m) << endl;
     }
-    //assert(false);
+    assert(false);
   }
 
   assert(false);
@@ -13190,7 +13234,7 @@ void cgra_flow_tests() {
   test_programs.push_back(unsharp());
   test_programs.push_back(conv_multi());
 
-  //test_schedules(test_programs);
+  test_schedules(test_programs);
 
   for (auto& prg : test_programs) {
     cout << "====== Running CGRA test for " << prg.name << endl;
