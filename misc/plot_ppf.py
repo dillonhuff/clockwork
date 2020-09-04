8adf4079e6c41b50e8ef1705ae4b22000b93e6f2
Dillon Huff <dillonhuff@gmail.com>
2020-09-04 12:26:04 -0700

    generalized plotting

--- misc/plot_ppf.py
+++ misc/plot_ppf.py
@@ -1,6 +1,18 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+patterns = [ "|" , "\\" , "/" , "+" , "-", ".", "*","x", "o", "O" ]
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(1, 1),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
 
 def set_size(w,h, ax=None):
     """ w, h: width, height in inches """
@@ -19,7 +31,7 @@ def set_size(w,h, ax=None):
 labels = ['MP', 'GP', 'SEF']
 men_means = [0.782, 0.78, 0.53]
 # women_means = [0.01, 0.0639, 0.00157]
-women_means = [0.01, 0.064, 0.0016]
+cpu_ppf = [0.01, 0.064, 0.0016]
 gpu_ppf = [2.24, 1.61, 0.22]
 k80_ppf = [1.61, 0.298, 0.0285]
 theoretical_peak = [1.14, 1.14, 1.14]
@@ -27,15 +39,28 @@ theoretical_peak = [1.14, 1.14, 1.14]
 x = np.arange(len(labels))  # the label locations
 width = 1.0
 margin = 0.1
+inter_group_margin = 0.05
+# bar_width = (width - margin - 2*inter_group_margin)/4.0
 bar_width = (width - margin)/4.0
 
-plt.axhline(y=THEORETICAL_PEAK, linewidth=2, color='red')
+categories = [men_means, gpu_ppf, k80_ppf, cpu_ppf]
+labels = ['CW', 'V100 GPU', 'K80 GPU', 'CPU']
+
+use_offset = len(categories) % 2 == 1
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - bar_width - bar_width/2.0, men_means, bar_width, label='CW', edgecolor='black')
-rects3 = ax.bar(x - 0         - bar_width/2.0, gpu_ppf, bar_width, label='V100 GPU', edgecolor='black')
-rects2 = ax.bar(x + bar_width - bar_width/2.0, women_means, bar_width, label='CPU', edgecolor='black')
-rects4 = ax.bar(x + 2*bar_width - bar_width/2.0, k80_ppf, bar_width, label='K80 GPU', edgecolor='black')
+offset = bar_width / 2.0 if use_offset else 0.0
+
+pos = -bar_width - offset
+rects = []
+for i in range(len(categories)):
+    rects.append(ax.bar(x - pos, categories[i], bar_width, label=labels[i], edgecolor='black'))
+    pos += bar_width
+
+# rects1 = ax.bar(x - bar_width - bar_width/2.0, men_means, bar_width, label='CW', edgecolor='black')
+# rects3 = ax.bar(x - 0         - bar_width/2.0, gpu_ppf, bar_width, label='V100 GPU', edgecolor='black')
+# rects2 = ax.bar(x + bar_width - bar_width/2.0, women_means, bar_width, label='CPU', edgecolor='black')
+# rects4 = ax.bar(x + 2*bar_width - bar_width/2.0, k80_ppf, bar_width, label='K80 GPU', edgecolor='black')
 
 THEORETICAL_PEAK = 1.14
 plt.axhline(y=THEORETICAL_PEAK, linewidth=2, color='red')
@@ -49,20 +74,12 @@ ax.legend()
 
 plt.yscale("log")
 
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(1, 1),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
+for c in rects:
+    autolabel(c)
+# autolabel(rects1)
+# autolabel(rects2)
+# autolabel(rects3)
+# autolabel(rects4)
 
 # fig.tight_layout()
 # set_size(5, 3)
