48799ce8f511ad1f70ce1c72ca88465cf4792ef0
Dillon Huff <dillonhuff@gmail.com>
2020-09-05 13:58:32 -0700

    parsing table

--- misc/parse_compile_times.py
+++ misc/parse_compile_times.py
@@ -4,5 +4,10 @@ f = open('./misc/compile_time_table.tex').readlines()
 
 for l in f:
     rm = "(.*)\\\\\\\\"
-    if re.match(rm, l):
+    m = re.match(rm, l)
+    if m:
         print(l)
+        values = m[1].split('&')
+        print('\t', values)
+
+
