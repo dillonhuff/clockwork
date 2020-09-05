a19a62c0ceb9c2c79e24d8eddf182d86928bfd05
Dillon Huff <dillonhuff@gmail.com>
2020-09-05 14:35:00 -0700

    experimenting with monochrome plots

--- misc/plot_bw.py
+++ misc/plot_bw.py
@@ -1,5 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from cycler import cycler
+
+# Create cycler object. Use any styling from above you please
+monochrome = (cycler('color', ['k']) * cycler('linestyle', ['-', ':', '--', '=.']) * cycler('marker', ['^',',', '.']))
 
 def set_size(w,h, ax=None):
     """ w, h: width, height in inches """
@@ -13,18 +17,29 @@ def set_size(w,h, ax=None):
     ax.figure.set_size_inches(figw, figh)
 
 # plt.style.use('seaborn-pastel')
-fig = plt.figure()
+# fig = plt.figure()
+
+fig, ax = plt.subplots(1,1)
+ax.set_prop_cycle(monochrome)
+ax.grid()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
 
 actual_bw = [0.88, 1.58, 3.06, 5.56, 11.2, 25.6]
 theoretical_peak_bw = [1, 2, 4, 8, 16, 30.72]
 
 requested_throughputs = [1, 2, 4, 8, 16, 32]
 
-# Create data
-# plot with matplotlib
-plt.plot(requested_throughputs, actual_bw, label='measured', marker='s')
-plt.plot(requested_throughputs, theoretical_peak_bw, label='expected', marker='o')
-plt.axhline(32, linewidth='2', color='red', label='PCIe peak bandwidth')
+plt.plot(requested_throughputs, actual_bw, label='measured')
+plt.plot(requested_throughputs, theoretical_peak_bw, label='expected')
+plt.axhline(32, linewidth='2', color='black', linestyle='-.', label='PCIe peak bandwidth')
+
+# plt.plot(requested_throughputs, actual_bw, label='measured', marker='s')
+# plt.plot(requested_throughputs, theoretical_peak_bw, label='expected', marker='o')
+# plt.axhline(32, linewidth='2', color='red', label='PCIe peak bandwidth')
+
 plt.ylabel('Bandwidth GB/sec')
 plt.xlabel('Requested Throughput (pix/cycle)')
 # set_size(5, 3)
