86b5de5ae0b121c8da1c9f199a105a22d588859f
Dillon Huff <dillonhuff@gmail.com>
2020-09-06 21:45:57 -0700

    creating parse script to find average improvement values

---
+++ misc/parse_resource_counts.py
@@ -0,0 +1,23 @@
+import re
+
+f = open('./misc/soda_comparison_table.tex').readlines()
+
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+lines = []
+for l in f:
+    rm = "(.*)\\\\\\\\"
+    m = re.match(rm, l)
+    if m:
+        values = m[1].split('&')
+        if is_float(values[1]):
+            print(l)
+            lines.append(values)
+
+
+print(lines)
--- misc/plot_ppf.py
+++ misc/plot_ppf.py
@@ -1,7 +1,23 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-patterns = [ "|" , "\\" , "/" , "+" , "-", ".", "*","x", "o", "O" ]
+from cycler import cycler
+
+plt.style.use('seaborn-pastel')
+
+
+hatch_patterns = [ "+" , "\\" , "/" , "+" , "-", ".", "*","x", "o", "O" ]
+# monochrome = (cycler('color', ['w']) * cycler('hatch', hatch_patterns))
+monochrome = (cycler('hatch', hatch_patterns))
+
+# plt.rcParams['axes.grid'] = True
+# # plt.rcParams['axes.prop_cycle'] = monochrome
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+# plt.rcParams['axes.spines.bottom'] = True
+# plt.rcParams['axes.spines.left'] = True
+
+
 
 def autolabel(rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
@@ -9,9 +25,10 @@ def autolabel(rects):
         height = rect.get_height()
         ax.annotate('{}'.format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(1, 1),  # 3 points vertical offset
+                    xytext=(0, 1),  # 3 points vertical offset
                     textcoords="offset points",
-                    ha='center', va='bottom')
+                    ha='center', va='bottom',
+                    zorder=20)
 
 
 def set_size(w,h, ax=None):
@@ -29,33 +46,60 @@ def set_size(w,h, ax=None):
 # matplotlib.style.use('tableau-colorblind10')
 
 labels = ['MP', 'GP', 'SEF']
-men_means = [0.782, 0.78, 0.53]
-# women_means = [0.01, 0.0639, 0.00157]
-cpu_ppf = [0.01, 0.064, 0.0016]
+
+cw_ppf = [0.782, 0.78, 0.53]
+cpu_ppf = [0.01, 0.064, 0.002]
 gpu_ppf = [2.24, 1.61, 0.22]
-k80_ppf = [1.61, 0.298, 0.0285]
+k80_ppf = [1.61, 0.298, 0.03]
+
+print('Improvement over k80 sef:', cw_ppf[2] / k80_ppf[2])
+print('Improvement over CPU sef:', cw_ppf[2] / cpu_ppf[2])
+
+# cw_ppf = [0.782, 0.78, 0.53]
+# cpu_ppf = [0.01, 0.064, 0.0016]
+# gpu_ppf = [2.24, 1.61, 0.22]
+# k80_ppf = [1.61, 0.298, 0.0285]
 theoretical_peak = [1.14, 1.14, 1.14]
 
 x = np.arange(len(labels))  # the label locations
-width = 1.0
-margin = 0.1
-inter_group_margin = 0.05
-# bar_width = (width - margin - 2*inter_group_margin)/4.0
-bar_width = (width - margin)/4.0
 
-categories = [men_means, gpu_ppf, k80_ppf, cpu_ppf]
-labels = ['CW', 'V100 GPU', 'K80 GPU', 'CPU']
+categories = [cw_ppf, gpu_ppf, k80_ppf, cpu_ppf]
+arch_labels = ['Clockwork', 'V100 GPU', 'K80 GPU', 'CPU']
+# categories.reverse()
+# arch_labels.reverse()
+
+
+width = 1.0
+margin = 0.2
+inter_bar_margin = 0.05
+# bar_width = (width - margin - 2*inter_group_margin)/4.0
+bar_width = (width - margin - inter_bar_margin*(len(categories) - 1))/4.0
+
 
 use_offset = len(categories) % 2 == 1
 
 fig, ax = plt.subplots()
+ax.grid(axis='y')
+# bar_cycle = (cycler('hatch', ['///', '--', '...','\///', 'xxx', '\\\\']) * cycler('edgecolor', 'k')*cycler('color', 'w')*cycler('zorder', [10]))
+# bar_cycle = (cycler('hatch', ['///', '--', '...','\///', 'xxx', '\\\\']) * cycler('edgecolor', 'k')*cycler('zorder', [10]))
+bar_cycle = (cycler('edgecolor', 'k')*cycler('zorder', [10]))
+styles = bar_cycle()
+# ax.set_prop_cycle(monochrome)
 offset = bar_width / 2.0 if use_offset else 0.0
 
-pos = -bar_width - offset
+# compute max position
+max_pos = -bar_width - offset - inter_bar_margin*3.0
+for i in range(len(categories) - 1):
+    max_pos += bar_width
+    max_pos += inter_bar_margin
+
+# pos = -bar_width - offset - inter_bar_margin*3.0
+pos = max_pos
 rects = []
 for i in range(len(categories)):
-    rects.append(ax.bar(x - pos, categories[i], bar_width, label=labels[i], edgecolor='black'))
-    pos += bar_width
+    rects.append(ax.bar(x - pos, categories[i], bar_width, **next(styles), label=arch_labels[i]))
+    pos -= bar_width
+    pos -= inter_bar_margin
 
 # rects1 = ax.bar(x - bar_width - bar_width/2.0, men_means, bar_width, label='CW', edgecolor='black')
 # rects3 = ax.bar(x - 0         - bar_width/2.0, gpu_ppf, bar_width, label='V100 GPU', edgecolor='black')
@@ -64,6 +108,7 @@ for i in range(len(categories)):
 
 THEORETICAL_PEAK = 1.14
 plt.axhline(y=THEORETICAL_PEAK, linewidth=2, color='red')
+plt.text(2.5, THEORETICAL_PEAK, 'Clockwork Theoretical Peak', ha='right', va='bottom')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('GPix / J')
@@ -72,7 +117,7 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
 
-plt.yscale("log")
+# plt.yscale("log")
 
 for c in rects:
     autolabel(c)
