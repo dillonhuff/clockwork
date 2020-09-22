c8188a551f7b1d88ee314ea55a77e9070db42985
Dillon Huff <dillonhuff@gmail.com>
2020-09-21 17:27:53 -0700

    all tests pass with if statement for wide fetch tile

--- coreir_backend.cpp
+++ coreir_backend.cpp
@@ -2663,9 +2663,6 @@ CoreIR::Module* delay_module(CodegenOptions& options,
 
     if (options.rtl_options.target_tile == TARGET_TILE_DUAL_SRAM_WITH_ADDRGEN) {
       generate_lake_collateral_delay_wdata_wrapped(mod->getName(), *verilog_collateral_file, D);
-      //generate_lake_collateral_delay_wdata_wrapped(mod->getName(), *verilog_collateral_file, D - 1);
-      //generate_lake_collateral_delay_wdata_wrapped(mod->getName(), *verilog_collateral_file, D - 2);
-      //generate_lake_collateral_delay_wdata_wrapped(mod->getName(), *verilog_collateral_file, D + 2);
     } else if (options.rtl_options.target_tile == TARGET_TILE_DUAL_SRAM_RAW) {
       auto def = mod->newModuleDef();
 
@@ -2721,7 +2718,7 @@ CoreIR::Module* delay_module(CodegenOptions& options,
       def->connect(bnk->sel("flush"), def->sel("self.flush"));
       mod->setDef(def);
     } else if (options.rtl_options.target_tile == TARGET_TILE_WIDE_FETCH_WITH_ADDRGEN) {
-      assert(false);
+      generate_lake_collateral_delay_wdata_wrapped(mod->getName(), *verilog_collateral_file, D);
     } else {
       assert(options.rtl_options.target_tile == TARGET_TILE_REGISTERS);
       auto def = mod->newModuleDef();
