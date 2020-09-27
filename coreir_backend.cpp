d54e6708fa99e5d71f78df442cfb4c76fd29a80e
Dillon Huff <dillonhuff@gmail.com>
2020-09-27 16:11:56 -0700

    with only bank selection on write we get an error

--- coreir_backend.cpp
+++ coreir_backend.cpp
@@ -480,6 +480,7 @@ void generate_platonic_ubuffer(
       num_banks *= val;
     }
     for (int b = 0; b < num_banks; b++) {
+      string source_ram = "bank_" + str(b);
       out << tab(3) << "if (" << buf.name << "_" << in << "_bank_selector.out == " << b << ") begin" << endl;
       out << tab(4) << source_ram << "[" << addr << "] <= " << buf.container_bundle(in) << "[" << buf.bundle_offset(in) << "]" << ";" << endl;
       out << tab(3) << "end" << endl;
