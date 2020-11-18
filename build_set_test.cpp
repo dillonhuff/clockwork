180e768e7976002dac051a6b32ac9471bbc04202
Dillon Huff <dillonhuff@gmail.com>
2020-11-18 11:54:34 -0800

    Restored BRAM codegen

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -17133,17 +17133,17 @@ void fpga_asplos_tests() {
 void cgra_flow_tests() {
   vector<prog> bram_test_programs{pointwise(), gaussian(), harris(), resnet()};
   test_codegen(bram_test_programs, compile_for_FPGA_BRAM_mem);
-  assert(false);
+  //assert(false);
 
   vector<prog> M1_test_programs = isca_programs();
   //vector<prog> M1_test_programs{pointwise()};
   test_codegen(M1_test_programs, compile_for_CGRA_M1_mem);
-  assert(false);
+  //assert(false);
 
   vector<prog> M3_test_programs = isca_programs();
   //vector<prog> M3_test_programs{up_sample(), resnet()};
   test_codegen(M3_test_programs, compile_for_CGRA_M3_mem);
-  assert(false);
+  //assert(false);
   
 
   auto test_programs =
