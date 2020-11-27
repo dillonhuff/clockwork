65260a3e7f1fe6eb30aa4888606dde87ba50ff65
Dillon Huff <dillonhuff@gmail.com>
2020-11-27 11:23:46 -0600

    Replacing for loops to find consumer data with helper function calls

--- prog.cpp
+++ prog.cpp
@@ -7824,17 +7824,18 @@ app_dag partition_application(const std::map<std::string, std::set<std::string> 
     cout << tab(1) << b.first << " is used by " << sep_list(b.second, "[", "]", ", ") << endl;
     auto consumers = prg.consumer_maps(b.first);
     for (auto group_name : b.second) {
-      vector<isl_set*> read;
-      for (auto m : consumers) {
-        if (m.second != nullptr && dag.in_group(m.first, group_name)) {
-          auto dom = range(m.second);
-          cout << tab(2) << group_name << " reads " << str(dom) << endl;
-          read.push_back(dom);
-        }
-      }
+      isl_set* s = read_by_group(b.first, group_name, dag);
+      //vector<isl_set*> read;
+      //for (auto m : consumers) {
+        //if (m.second != nullptr && dag.in_group(m.first, group_name)) {
+          //auto dom = range(m.second);
+          //cout << tab(2) << group_name << " reads " << str(dom) << endl;
+          //read.push_back(dom);
+        //}
+      //}
 
 
-      isl_set* s = unn(read);
+      //isl_set* s = unn(read);
       cout << tab(2) << "Read: " << str(lexmin(s)) << " to " << str(lexmax(s)) << endl;
       assert(contains_key(group_name, dag.fusion_group_progs));
       prog& gp = dag.fusion_group_progs.at(group_name);
