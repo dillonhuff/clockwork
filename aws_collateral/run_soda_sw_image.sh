b6fcc585bea14729cd45ee0a710c5e946ef43ddc
Cloud User <centos@ip-172-31-52-41.ec2.internal>
2020-12-14 20:38:46 +0000

    Putting together soda codes

--- aws_collateral/run_soda_sw_image.sh
+++ aws_collateral/run_soda_sw_image.sh
@@ -1,3 +1,4 @@
 ../../../aws_collateral/rebuild_host.sh soda_${app}_host.cpp || { echo 'failed to build host'; exit 1; }
+./build_kernel.sh
 make check TARGET=sw_emu TARGET=$AWS_PLATFORM all
 echo 'done'
