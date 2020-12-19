4d3c85775fafd276a9224142beeafd9bf55b2176
Dillon Huff <dillonhuff@gmail.com>
2020-12-18 23:47:04 -0600

    added ic300

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -9148,7 +9148,7 @@ void stencil_chain_20_stage_iccad_apps(const std::string& prefix) {
     CodegenOptions options;
     options.internal = true;
     options.use_custom_code_string = true;
-    options.rtl_options.hls_clock_target_Hz = 400000000;
+    options.rtl_options.hls_clock_target_Hz = 300000000;
     lp.realize(options, name, {cols, rows}, "in", throughput);
 
     move_to_benchmarks_folder(name + "_opt");
@@ -11224,7 +11224,8 @@ void iccad_tests() {
 
   //stencil_chain_iccad_apps("icsc_500MHz");
   //stencil_chain_20_stage_iccad_apps("ic20_500MHz");
-  stencil_chain_20_stage_iccad_apps("ic20_400MHz");
+  //stencil_chain_20_stage_iccad_apps("ic20_400MHz");
+  stencil_chain_20_stage_iccad_apps("ic20_300MHz");
   stencil_chain_iccad_apps("icsc_500MHz");
   stencil_chain_fan_out_iccad_apps("icfo");
   stencil_chain_eight_stage_iccad_apps("icsc_8s");
---
+++ soda_codes/ic20_300MHz_1_opt/set_app.sh
@@ -0,0 +1,2 @@
+export app=ic20_300MHz_1_opt
+export HLS_CLOCK_FREQUENCY=300000000
