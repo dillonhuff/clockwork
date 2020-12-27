5ae71fe016d62a141a5002071a5626c00b20fe8d
Cloud User <centos@ip-172-31-52-41.ec2.internal>
2020-12-27 21:42:24 +0000

    Re-structured soda comparison scripts

--- aws_collateral/run_soda_comparison.sh
+++ aws_collateral/run_soda_comparison.sh
@@ -1,9 +1,12 @@
-APP=$1
+APP_N=$1
+APP=${APP_N}_opt
 cd ./soda_codes/${APP}/
 source ./set_app.sh
 cd ../../
 
 cd ./soda_codes/${APP}/soda_code/
+
+#./build_kernel.sh
 ../../../aws_collateral/run_soda_sw_image.sh
 
 cd ../
@@ -14,4 +17,4 @@ g++ regression_tb_${APP}.cpp -I ../../../ -std=c++11 ${APP}.cpp -D__INT_OUTPUT__
 
 cd ../../../
 
-./run_aligner.sh ./soda_codes/${APP}/our_code/regression_result_${APP}.txt ./soda_codes/${APP}/soda_code/${APP}_update_0_write_accel_result.csv
+./run_aligner.sh ./soda_codes/${APP}/our_code/regression_result_${APP}.txt ./soda_codes/${APP}/soda_code/${APP_N}_update_0_write_accel_result.csv
