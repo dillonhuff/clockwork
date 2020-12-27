6b3b3b7276904fb82dfa3e7bc130ac900672e8b9
Dillon Huff <dillonhuff@gmail.com>
2020-12-27 13:16:20 -0600

    Added ic15 1 and 16 pix

---
+++ soda_codes/ic15_300MHz_16_opt/soda_code/tb_soda_ic15_300MHz_16_opt.cpp
@@ -0,0 +1,34 @@
+// AUTO GEN SODA TB
+#include "ic15_300MHz_16_opt_kernel.h"
+#include <iostream>
+#include <fstream>
+
+#define PIXEL_WIDTH 16
+#define BURST_WIDTH 256
+
+#include "runtime/test_utils.h"
+
+using namespace std;
+
+int main() {
+  srand(234);
+  const int nrows = 1095;
+  const int ncols = 2400;
+  uint64_t img_pixels = nrows*ncols;
+  const uint64_t bits_per_pixel = PIXEL_WIDTH;
+  uint64_t img_bits = bits_per_pixel*img_pixels;
+  const uint64_t num_transfers = img_bits / BURST_WIDTH;
+  const uint64_t pixels_per_burst = BURST_WIDTH / bits_per_pixel;
+
+  cout << "num transfers    : " << num_transfers << endl;
+  cout << "pixels / transfer: " << pixels_per_burst << endl;
+
+  const uint64_t transfer_cols = ncols / pixels_per_burst;
+  ap_uint<BURST_WIDTH>* ic15_300MHz_16 = (ap_uint<BURST_WIDTH>*) malloc(sizeof(ap_uint<BURST_WIDTH>)*num_transfers);
+  ap_uint<BURST_WIDTH>* in_off_chip = (ap_uint<BURST_WIDTH>*) malloc(sizeof(ap_uint<BURST_WIDTH>)*num_transfers);
+  fill_array_decimal<bits_per_pixel>("in_off_chip_input_pixel.csv", in_off_chip, nrows, ncols, transfer_cols);
+  ic15_300MHz_16_opt_kernel(ic15_300MHz_16, in_off_chip, num_transfers);
+  write_results_decimal<bits_per_pixel>("soda_ic15_300MHz_16_opt_regression_result.csv", ic15_300MHz_16, nrows, ncols, transfer_cols);
+  free(in_off_chip);
+  free(ic15_300MHz_16);
+}
---
+++ soda_codes/ic15_300MHz_1_opt/set_app.sh
@@ -0,0 +1,2 @@
+export app=ic15_300MHz_1_opt
+export HLS_CLOCK_FREQUENCY=300000000
