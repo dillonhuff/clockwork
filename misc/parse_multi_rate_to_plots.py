29c11e20e630d895ded471470a9d33d54c16ad6c
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-01-05 01:09:44 -0700

    Adding parsing for multi-rate tables

--- misc/parse_multi_rate_to_plots.py
+++ misc/parse_multi_rate_to_plots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # f = open('./misc/soda_resource_comparison_table.tex').readlines()
-f = open('./misc/soda_comparison_standalone_large.tex').readlines()
+f = open('./misc/multi_rate_standalone.tex').readlines()
 
 def intersperse(lst, item):
     result = [item] * (len(lst) * 2 - 1)
@@ -25,86 +25,87 @@ def table_op(table_lines, func):
         m = re.match(rm, l)
         if m:
             values = m[1].split('&')
-            if is_float(values[1]):
+            if is_float(values[1]) or values[1].strip() == 'HLS':
                 stripped = [element.strip() for element in values]
                 lines.append(stripped)
     print(lines)
 
-    app_names = ['Blur', 'CP', 'Sobel', 'Jacobi']
-    systems = ['SODA', 'CW']
-    resources = ['LUT', 'BRAM', 'FF']
+    # app_names = ['Blur', 'CP', 'Sobel', 'Jacobi']
+    # systems = ['SODA', 'CW']
+    # resources = ['LUT', 'BRAM', 'FF']
 
-    apps = {}
-    for resource in resources:
-        apps[resource] = {}
-        for app in app_names:
-            apps[resource][app] = {}
-            for system in systems:
-                apps[resource][app][system] = []
+    # apps = {}
+    # for resource in resources:
+        # apps[resource] = {}
+        # for app in app_names:
+            # apps[resource][app] = {}
+            # for system in systems:
+                # apps[resource][app][system] = []
     
     for l in lines:
-        apps['LUT'][l[0]][l[2]].append(float(l[3]))
-        apps['FF'][l[0]][l[2]].append(float(l[5]))
-        apps['BRAM'][l[0]][l[2]].append(float(l[6]))
+        print(l)
+        # apps['LUT'][l[0]][l[2]].append(float(l[3]))
+        # apps['FF'][l[0]][l[2]].append(float(l[5]))
+        # apps['BRAM'][l[0]][l[2]].append(float(l[6]))
 
-    print(apps)
-    # assert(False)
+    # print(apps)
+    # # assert(False)
 
-    NumApps = len(app_names)
-    N = len(apps[resource][app]['SODA'])
-    fig, ax = plt.subplots(N, NumApps, sharex='col', sharey='row')
+    # NumApps = len(app_names)
+    # N = len(apps[resource][app]['SODA'])
+    # fig, ax = plt.subplots(N, NumApps, sharex='col', sharey='row')
 
-    # fig.suptitle('Resource Utilization for SODA and Clockwork (CW)')
-    i = 0
-    for resource in resources:
-        j = 0
-        for app in app_names:
-            print('resource = {0}'.format(resource))
-            print('app      = {0}'.format(app))
-            print(apps[resource])
-            print(apps[resource][app])
-            assert(apps[resource] != None)
-            menMeans = apps[resource][app]['SODA']
-
-            ind = np.arange(N)  # the x locations for the groups
-            width = 0.35       # the width of the bars
-
-            # fig = plt.figure()
-            # ax[i, j] = fig.add_subplot(111)
-            rects1 = ax[i, j].bar(ind, menMeans, width)
-
-            womenMeans= apps[resource][app]['CW']
-            rects2 = ax[i, j].bar(ind+width, womenMeans, width)
-
-            # add some
-            # ax[i, j].set_ylabel('Counts')
-            # ax[i, j].set_title('{0} count for {1} by throughput'.format(resource, app))
-            ax[i, j].set_xticks(ind + width / 2)
-            ax[i, j].set_xticklabels( ('{0} 1'.format(app), '{0} 16'.format(app), '{0} 32'.format(app)))
-
-            if i == 0 and j == 0:
-                ax[i, j].legend( (rects1[0], rects2[0]), ('SODA', 'CW') )
-
-            j += 1
-        i += 1
-
-    ax.flat[0].set(ylabel='LUT')
-    ax.flat[NumApps].set(ylabel='BRAM')
-    ax.flat[2*NumApps].set(ylabel='FF')
-    # ax.flat[1].set(ylabel='BRAM')
-    # ax.flat[2].set(ylabel='FF')
+    # # fig.suptitle('Resource Utilization for SODA and Clockwork (CW)')
     # i = 0
-    # for axi in ax.flat:
-        # print('axi =', i)
-        # axi.set(ylabel='LUT')
+    # for resource in resources:
+        # j = 0
+        # for app in app_names:
+            # print('resource = {0}'.format(resource))
+            # print('app      = {0}'.format(app))
+            # print(apps[resource])
+            # print(apps[resource][app])
+            # assert(apps[resource] != None)
+            # menMeans = apps[resource][app]['SODA']
+
+            # ind = np.arange(N)  # the x locations for the groups
+            # width = 0.35       # the width of the bars
+
+            # # fig = plt.figure()
+            # # ax[i, j] = fig.add_subplot(111)
+            # rects1 = ax[i, j].bar(ind, menMeans, width)
+
+            # womenMeans= apps[resource][app]['CW']
+            # rects2 = ax[i, j].bar(ind+width, womenMeans, width)
+
+            # # add some
+            # # ax[i, j].set_ylabel('Counts')
+            # # ax[i, j].set_title('{0} count for {1} by throughput'.format(resource, app))
+            # ax[i, j].set_xticks(ind + width / 2)
+            # ax[i, j].set_xticklabels( ('{0} 1'.format(app), '{0} 16'.format(app), '{0} 32'.format(app)))
+
+            # if i == 0 and j == 0:
+                # ax[i, j].legend( (rects1[0], rects2[0]), ('SODA', 'CW') )
+
+            # j += 1
         # i += 1
 
-    for axi in ax.flat:
-        axi.label_outer()
+    # ax.flat[0].set(ylabel='LUT')
+    # ax.flat[NumApps].set(ylabel='BRAM')
+    # ax.flat[2*NumApps].set(ylabel='FF')
+    # # ax.flat[1].set(ylabel='BRAM')
+    # # ax.flat[2].set(ylabel='FF')
+    # # i = 0
+    # # for axi in ax.flat:
+        # # print('axi =', i)
+        # # axi.set(ylabel='LUT')
+        # # i += 1
 
-    fig.set_size_inches(16, 10)
-    plt.show()
-    fig.savefig('clockwork_soda_bar_charts.eps', format='eps')
+    # for axi in ax.flat:
+        # axi.label_outer()
+
+    # fig.set_size_inches(16, 10)
+    # plt.show()
+    # fig.savefig('clockwork_soda_bar_charts.eps', format='eps')
 
 def sum_double_entry(values):
     rm = "\s*(\d+)\s+(\d+)\s*"
