cf5abad669afa939e3392d599c80ec3881b952ad
Cloud User <centos@ip-172-31-52-41.ec2.internal>
2021-02-02 01:44:49 +0000

    epochs

--- soda_codes/pyr_blnd2d500_2048/our_code/pyr_blnd2d500_2048_host.cpp
+++ soda_codes/pyr_blnd2d500_2048/our_code/pyr_blnd2d500_2048_host.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
 
   std::string binaryFile = argv[1];
 
-  int num_epochs = 1;
+  int num_epochs = 10;
 
   std::cout << "num_epochs = " << num_epochs << std::endl;
 
