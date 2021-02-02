6b8cfab6f097ab717ac9a9ac7cef1eabb66e0315
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-02-02 12:20:41 -0700

    Added sbl 32

---
+++ soda_codes/sbl_d_32_opt/our_code/regression_tb_sbl_d_32_opt.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include "sbl_d_32_opt.h"
+
+int main() {
+  srand(234);
+  ofstream fout("regression_result_sbl_d_32_opt.txt");
+  HWStream<hw_uint<16 > > off_chip_img;
+  HWStream<hw_uint<16 > > sbl_d_32;
+
+
+  // Loading input data
+  srand(1);
+  // cmap    : { img_update_0[root = 0, img_0, img_1] -> off_chip_img[img_1, img_0] : -1 <= img_0 <= 1080 and -1 <= img_1 <= 1080 }
+  // read map: { off_chip_img[i0, i1] -> img_update_0[root = 0, img_0 = i1, img_1 = i0] : -1 <= i0 <= 1080 and -1 <= i1 <= 1080 }
+  // rng     : { img_update_0[root = 0, img_0, img_1] : -1 <= img_0 <= 1080 and -1 <= img_1 <= 1080 }
+  // rng card: { 1170724 }
+  for (int i = 0; i < 1170724; i++) {
+    hw_uint<16 > value;
+    set_at<0, 16, 16>(value, rand() % 256);
+    off_chip_img.write(value);
+  }
+
+  sbl_d_32_opt(off_chip_img, sbl_d_32);
+  for (int i = 0; i < 1166400; i++) {
+    auto actual = sbl_d_32.read();
+    hw_uint<16> actual_lane_0 = actual.extract<0, 15>();
+    fout << actual_lane_0 << endl;
+  }
+
+  return 0;
+}
---
+++ soda_codes/sbl_d_32_opt/our_code/sbl_d_32_opt_compute_units.h_merged_compute_units.h
@@ -0,0 +1,2 @@
+#include "sbl_d_32_opt_compute_units.h"
+
