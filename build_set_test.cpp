3361cebf1ab9f78ce18b5aceaee8a634e9bab0b7
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-02-04 09:16:44 -0700

    Added 16 wide cp opt

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -19658,6 +19658,62 @@ void jac16_static_dynamic_comparison() {
 
 }
 
+void cp16_static_dynamic_comparison() {
+  string prefix = "cp";
+
+  int size = 1080;
+  int rows = size;
+  int cols = size;
+
+  int unroll_factor = 16;
+  int throughput = unroll_factor;
+  string out_name = prefix + "_" + str(unroll_factor);
+
+  CodegenOptions options;
+  options.internal = true;
+  options.hls_loop_codegen = HLS_LOOP_CODEGEN_PERFECT;
+  options.debug_options.expect_all_linebuffers = true;
+
+  App jac = camera_pipeline(out_name);
+  prog prg = jac.realize(options, out_name, cols, rows, 1);
+
+  move_to_benchmarks_folder(out_name + "_opt");
+
+  prg.name = out_name + "_opt_d";
+
+  jac.generate_soda_file(prg.name, throughput);
+
+  unroll_reduce_loops(prg);
+  merge_basic_block_ops(prg);
+  normalize_bounds(prg);
+  normalize_address_offsets(prg);
+
+  auto fusion_groups = one_stage_per_group(prg);
+  auto fresh_groups = insert_inter_group_buffers(fusion_groups, prg);
+  unroll_mismatched_inner_loops(prg);
+  merge_basic_block_ops(prg);
+  infer_bounds_and_unroll(pick(prg.outs), {size, size}, throughput, prg);
+
+  assert(unoptimized_compiles(prg));
+
+  app_dag dag = partition_groups(fresh_groups, prg);
+
+  options = CodegenOptions();
+  options.hls_loop_codegen = HLS_LOOP_CODEGEN_PERFECT;
+  generate_app_code(options, dag);
+
+  move_to_benchmarks_folder(prg.name);
+
+  string synth_dir =
+    "./soda_codes/" + prg.name+ "/our_code/";
+
+  system(("cp " + out_name + "_opt" + "*.h " + synth_dir).c_str());
+
+  cout << "prg name: " << prg.name << endl;
+
+  assert(false);
+}
+
 void cp_static_dynamic_comparison() {
   string prefix = "cp";
 
@@ -20011,6 +20067,7 @@ void sbl_static_dynamic_comparison() {
 
 void initial_soda_comparison() {
 
+  cp16_static_dynamic_comparison();
   cp_static_dynamic_comparison();
 
 
---
+++ soda_codes/cp_16_opt_d/our_code/cp_16_opt_compute_units.h_merged_compute_units.h
@@ -0,0 +1,2 @@
+#include "cp_16_opt_compute_units.h"
+
