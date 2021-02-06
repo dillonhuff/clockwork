a3f4c18793d7edbbcff3802aa16d87aaa26abdc2
joyliu37 <joyliu37@sina.com>
2021-02-05 16:11:23 -0800

    three level mem stuck at scheduling

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -14261,9 +14261,9 @@ void cpy_app_to_folder(const std::string& app_type, const std::string& prg_name)
 
 void test_pond(string dir) {
   vector<prog> test_apps;
+  test_apps.push_back(three_level_memory());
   test_apps.push_back(resnet_simple());
   test_apps.push_back(resnet());
-  test_apps.push_back(three_level_memory());
 
   for ( auto prg: test_apps) {
     cout << "====== Running CGRA Single Port test for " << prg.name << endl;
