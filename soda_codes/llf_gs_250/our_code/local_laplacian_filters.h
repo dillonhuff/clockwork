fdebf44663c012324c0de2fac1b2a0f89f823ef1
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-02-23 15:25:56 -0700

    Added llf

---
+++ soda_codes/llf_gs_250/our_code/local_laplacian_filters.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+#include "hw_classes.h"
+
+void local_laplacian_filters(HWStream<hw_uint<32> >& /* no bundle get_args num ports = 1 */gray_in_oc, HWStream<hw_uint<32> >& /* no bundle get_args num ports = 1 */gray_out);
