4d268fec571bfe78fa6581adf240f8ae48b37e7e
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-02-24 16:43:13 -0700

    Adding blur4 with 1920 wide input

---
+++ soda_codes/blur4_1_opt/our_code/blur4_1_opt_dis.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+#include "hw_classes.h"
+
+void blur4_1_opt_dis(HWStream<hw_uint<16> >& /* no bundle get_args num ports = 1 */input_arg, HWStream<hw_uint<16> >& /* get_args num ports = 1 */blur4_1);
