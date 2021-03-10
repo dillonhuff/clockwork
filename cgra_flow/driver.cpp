4b847663cd636df46c0ceb99f72ac6c1905219d0
joyliu37 <joyliu37@sina.com>
2021-03-10 10:29:19 -0800

    decouple cgra flow into multiple steps

--- cgra_flow/driver.cpp
+++ cgra_flow/driver.cpp
@@ -4,7 +4,17 @@
 int main(int argc, char** argv) {
     auto prg = conv_3_3();
     string dir = "./cgra_flow/result";
+    preprocess_prog(prg);
+
+    //Run Frontend Test, generate gold TB
+    auto cpu = unoptimized_result(prg);
+
+    //Run Memory Mapper and dump collateral into dir
     compile_app_for_garnet_single_port_mem(prg, dir);
+    //Run interconnect agnostic tb
+    auto cgra = cgra_flow_result(prg, dir);
+
+    sanity_check(prg, cpu, cgra);
 
     return 0;
 }
