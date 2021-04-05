62dad5365fbe40ab8bdce2241f079087b96225aa
joyliu37 <joyliu37@sina.com>
2021-04-05 10:49:03 -0700

    test pass for new pond-wrapper

--- build_set_test.cpp
+++ build_set_test.cpp
@@ -14280,10 +14280,10 @@ void cpy_app_to_folder(const std::string& app_type, const std::string& prg_name)
 
 void test_pond(string dir) {
   vector<prog> test_apps;
-  //test_apps.push_back(three_level_pond_rolled());
-  //test_apps.push_back(resnet_simple());
-  //test_apps.push_back(resnet());
-  //test_apps.push_back(three_level_pond_copy());
+  test_apps.push_back(three_level_pond_rolled());
+  test_apps.push_back(resnet_simple());
+  test_apps.push_back(resnet());
+  test_apps.push_back(three_level_pond_copy());
   test_apps.push_back(fft8_unroll8_split());
 
   //TODO: tobe tested with new pond
