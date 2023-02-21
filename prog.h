c376b903b6b88c1cad287edfd483488f7514e618
root <melchert@stanford.edu>
2023-02-20 16:24:27 -0800

    Using natural sort for compute kernel ids

--- prog.h
+++ prog.h
@@ -974,9 +974,48 @@ struct ir_node {
 
 };
 
-struct cmp_op {
+
+struct cmp_op { 
+  std::string toUpper(std::string s) const {
+    for(int i=0;i<(int)s.length();i++){s[i]=toupper(s[i]);}
+    return s;
+  }
+
+  bool compareNat(const std::string& a, const std::string& b) const {
+    if (a.empty())
+        return true;
+    if (b.empty())
+        return false;
+    if (std::isdigit(a[0]) && !std::isdigit(b[0]))
+        return true;
+    if (!std::isdigit(a[0]) && std::isdigit(b[0]))
+        return false;
+    if (!std::isdigit(a[0]) && !std::isdigit(b[0]))
+    {
+        if (a[0] == b[0])
+            return compareNat(a.substr(1), b.substr(1));
+        return (toUpper(a) < toUpper(b));
+        //toUpper() is a function to convert a std::string to uppercase.
+    }
+
+    // Both strings begin with digit --> parse both numbers
+    std::istringstream issa(a);
+    std::istringstream issb(b);
+    int ia, ib;
+    issa >> ia;
+    issb >> ib;
+    if (ia != ib)
+        return ia < ib;
+
+    // Numbers are the same --> remove numbers and recurse
+    std::string anew, bnew;
+    std::getline(issa, anew);
+    std::getline(issb, bnew);
+    return (compareNat(anew, bnew));
+  }
+
   bool operator() (op* l, op* r) const {
-    return l->name < r->name;
+    return compareNat(l->name, r->name);
   }
 };
 
